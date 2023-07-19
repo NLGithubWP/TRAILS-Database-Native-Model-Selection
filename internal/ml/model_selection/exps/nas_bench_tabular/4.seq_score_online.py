65699dedd0d502fe21cd95a4d7650610156561c7
working <57171759+NLGithubWP@users.noreply.github.com>
2023-07-19 20:15:48 +0800

    Compatiable with image data

--- internal/ml/model_selection/exps/nas_bench_tabular/4.seq_score_online.py
+++ internal/ml/model_selection/exps/nas_bench_tabular/4.seq_score_online.py
@@ -27,23 +27,6 @@ def generate_data_loader():
     return train_loader, val_loader, test_loader, class_num
 
 
-def run_with_time_budget(time_budget: float):
-    """
-    :param time_budget: the given time budget, in second
-    :return:
-    """
-
-    # define dataLoader, and sample a mini-batch
-    train_loader, val_loader, test_loader, class_num = generate_data_loader()
-    args.num_labels = class_num
-    data_loader = [train_loader, val_loader, test_loader]
-
-    rms = RunModelSelection(args.search_space, args, is_simulate=True)
-    best_arch, best_arch_performance, _, _, _ = rms.select_model_online(time_budget, data_loader)
-
-    return best_arch, best_arch_performance
-
-
 if __name__ == "__main__":
     args = parse_arguments()
 
@@ -63,11 +46,11 @@ if __name__ == "__main__":
     from src.search_space.init_search_space import init_search_space
     from src.storage.structure_data_loader import libsvm_dataloader
     from src.utilslibs.io_tools import write_json, read_json
-    from src.eva_engine.run_ms import RunModelSelection
     from src.storage import dataset
+    from src.common.constant import Config, CommonVars
 
     search_space_ins = init_search_space(args)
-    search_space_ins.load()
+
     train_loader, val_loader, test_loader, class_num = generate_data_loader()
 
     _evaluator = P1Evaluator(device=args.device,
@@ -75,7 +58,8 @@ if __name__ == "__main__":
                              dataset_name=args.dataset,
                              search_space_ins=search_space_ins,
                              train_loader=train_loader,
-                             is_simulate=False)
+                             is_simulate=False,
+                             metrics=CommonVars.ALL_EVALUATOR)
 
     sampler = SequenceSampler(search_space_ins)
 
@@ -84,27 +68,28 @@ if __name__ == "__main__":
     result = read_json(output_file)
     print(f"begin to score all, currently we already explored {len(result.keys())}")
     while True:
-        arch_micro, _ = sampler.sample_next_arch()
-        if arch_micro is None:
+        arch_id, arch_micro = sampler.sample_next_arch()
+        if arch_id is None:
             break
-        if arch_micro in result:
+        if arch_id in result:
             continue
         # run the model selection
-        model_acquire_data = ModelAcquireData(model_id=str(arch_micro),
-                                              model_encoding=arch_micro,
+        model_encoding = search_space_ins.serialize_model_encoding(arch_micro)
+        model_acquire_data = ModelAcquireData(model_id=arch_id,
+                                              model_encoding=model_encoding,
                                               is_last=False)
         data_str = model_acquire_data.serialize_model()
         model_score = _evaluator.p1_evaluate(data_str)
         explored_n += 1
-        result[arch_micro] = model_score
-        print(f" {datetime.now()}finish arch = {arch_micro}")
+        result[arch_id] = model_score
+        print(f" {datetime.now()}finish arch = {arch_id}")
         if explored_n % 100 == 0:
             print("3. [trails] Phase 1: filter phase explored " + str(explored_n)
                   + "Total explored" + str(len(result)) +
-                  " model, model_id = " + str(arch_micro) +
+                  " model, model_id = " + str(arch_id) +
                   " model_scores = " + json.dumps(model_score))
             logger.info("3. [trails] Phase 1: filter phase explored " + str(explored_n) +
-                        " model, model_id = " + str(arch_micro) +
+                        " model, model_id = " + str(arch_id) +
                         " model_scores = " + json.dumps(model_score))
             write_json(output_file, result)
     write_json(output_file, result)
