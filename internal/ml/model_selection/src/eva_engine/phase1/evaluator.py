5fa318d6fb22efb126a850cac2893dcdf0f3c90c
working <57171759+NLGithubWP@users.noreply.github.com>
2023-07-20 13:48:38 +0800

    Update evaluator

--- internal/ml/model_selection/src/eva_engine/phase1/evaluator.py
+++ internal/ml/model_selection/src/eva_engine/phase1/evaluator.py
@@ -10,8 +10,18 @@ from torch.utils.data import DataLoader
 class P1Evaluator:
 
     def __init__(self, device: str, num_label: int, dataset_name: str, search_space_ins,
-                 train_loader: DataLoader, is_simulate: bool):
-
+                 train_loader: DataLoader, is_simulate: bool, metrics: str = CommonVars.ExpressFlow):
+        """
+        :param device:
+        :param num_label:
+        :param dataset_name:
+        :param search_space_ins:
+        :param search_space_ins:
+        :param train_loader:
+        :param is_simulate:
+        :param metrics: which TFMEM to use?
+        """
+        self.metrics = metrics
         self.is_simulate = is_simulate
 
         self.dataset_name = dataset_name
@@ -24,7 +34,7 @@ class P1Evaluator:
 
         self.score_getter = None
 
-        # load the data loader
+        # get one mini batch
         if self.dataset_name in [Config.c10, Config.c100, Config.imgNet]:
             # for img data
             self.mini_batch, self.mini_batch_targets = dataset.get_mini_batch(
@@ -32,6 +42,8 @@ class P1Evaluator:
                 sample_alg="random",
                 batch_size=32,
                 num_classes=self.num_labels)
+            self.mini_batch.to(self.device)
+            self.mini_batch_targets.to(self.device)
         elif self.dataset_name in [Config.Criteo, Config.Frappe, Config.UCIDataset]:
             # this is structure data
             batch = iter(self.train_loader).__next__()
@@ -52,27 +64,11 @@ class P1Evaluator:
         model_acquire = ModelAcquireData.deserialize(data_str)
 
         if self.is_simulate:
-            return self._p1_evaluate_simu(model_acquire.model_encoding)
+            return self._p1_evaluate_simu(model_acquire)
         else:
-            return self._p1_evaluate_online(model_acquire.model_encoding)
+            return self._p1_evaluate_online(model_acquire)
 
-    def _p1_evaluate_online(self, model_encoding: str) -> dict:
-
-        # score all tfmem
-        # model_score = {}
-        # for alg, score_evaluator in evaluator_register.items():
-        #     if alg == CommonVars.PRUNE_SYNFLOW or alg == CommonVars.ExpressFlow:
-        #         bn = False
-        #     else:
-        #         bn = True
-        #     new_model = self.search_space_ins.new_arch_scratch_with_default_setting(model_encoding, bn=bn)
-        #     new_model = new_model.to(self.device)
-        #     naswot_score, _ = score_evaluator.evaluate_wrapper(
-        #         arch=new_model,
-        #         device=self.device,
-        #         batch_data=self.mini_batch,
-        #         batch_labels=self.mini_batch_targets)
-        #     model_score[alg] = naswot_score
+    def _p1_evaluate_online(self, model_acquire: ModelAcquireData) -> dict:
 
         # # 1. Score NasWot
         # new_model = self.search_space_ins.new_arch_scratch_with_default_setting(model_encoding, bn=True)
@@ -96,21 +92,67 @@ class P1Evaluator:
         # model_score = {CommonVars.NAS_WOT: naswot_score,
         #                CommonVars.PRUNE_SYNFLOW: synflow_score}
 
-        # 2. Score ExpressFLow
-        new_model = self.search_space_ins.new_arch_scratch_with_default_setting(model_encoding, bn=False)
-        new_model = new_model.to(self.device)
-        expressFlow_score, _ = evaluator_register[CommonVars.ExpressFlow].evaluate_wrapper(
-            arch=new_model,
-            device=self.device,
-            batch_data=self.mini_batch,
-            batch_labels=self.mini_batch_targets)
+        model_encoding = model_acquire.model_encoding
+        # score all tfmem
+        if self.metrics == CommonVars.ALL_EVALUATOR:
+            model_score = {}
+            for alg, score_evaluator in evaluator_register.items():
+                if alg == CommonVars.PRUNE_SYNFLOW or alg == CommonVars.ExpressFlow:
+                    bn = False
+                else:
+                    bn = True
+                new_model = self.search_space_ins.new_arch_scratch_with_default_setting(model_encoding, bn=bn)
+                new_model = new_model.to(self.device)
+                naswot_score, _ = score_evaluator.evaluate_wrapper(
+                    arch=new_model,
+                    device=self.device,
+                    batch_data=self.mini_batch,
+                    batch_labels=self.mini_batch_targets)
+                model_score[alg] = naswot_score
 
-        model_score = {CommonVars.ExpressFlow: expressFlow_score}
+                # clear the cache
+                del new_model
+                if "cuda" in self.device:
+                    torch.cuda.empty_cache()
+        else:
+            # score using only one metrics
+            if self.metrics == CommonVars.PRUNE_SYNFLOW or self.metrics == CommonVars.ExpressFlow:
+                bn = False
+            else:
+                bn = True
+            new_model = self.search_space_ins.new_arch_scratch_with_default_setting(model_encoding, bn=bn)
+            new_model = new_model.to(self.device)
+
+            _score, _ = evaluator_register[self.metrics].evaluate_wrapper(
+                arch=new_model,
+                device=self.device,
+                batch_data=self.mini_batch,
+                batch_labels=self.mini_batch_targets)
+
+            model_score = {self.metrics: _score}
+
+            del new_model
+            if "cuda" in self.device:
+                torch.cuda.empty_cache()
 
         return model_score
 
-    def _p1_evaluate_simu(self, model_encoding: str) -> dict:
+    def _p1_evaluate_simu(self, model_acquire: ModelAcquireData) -> dict:
         if self.score_getter is None:
             self.score_getter = SimulateScore(space_name=self.search_space_ins.name)
-        model_score = self.score_getter.get_score_res(arch_id=model_encoding, dataset=self.dataset_name)
+
+        model_score = self.score_getter.get_score_res(
+            arch_id=model_acquire.model_id,
+            dataset=self.dataset_name)
+
         return model_score
+
+    def measure_flops(self):
+        pass
+        # from thop import clever_format,profile
+        # flops, params = profile(new_arch, inputs=(mini_batch,))
+        # flops2, params2 = clever_format([flops, params], "%.3f")
+        # print('FLOPs = ' + str(flops / 1000 ** 3) + 'G')
+        # print('Params = ' + str(params / 1000 ** 2) + 'M')
+        # print("origin: ", f"arch_id={3380}, B={args.batch_size},C={args.init_channels}, flops2={flops2}，
+        # params2={params2}")
