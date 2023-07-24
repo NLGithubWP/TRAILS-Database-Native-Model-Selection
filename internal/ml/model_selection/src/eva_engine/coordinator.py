61b8f0dafe49dcc10a0cadcc9f0a2808deb524de
working <57171759+NLGithubWP@users.noreply.github.com>
2023-07-24 12:25:02 +0800

    Add comments to coordinator

--- internal/ml/model_selection/src/eva_engine/coordinator.py
+++ internal/ml/model_selection/src/eva_engine/coordinator.py
@@ -1,42 +1,22 @@
 from src.common.constant import Config
 from src.eva_engine.phase2.run_sh import BudgetAwareControllerSH
 from src.logger import logger
+from src.search_space.core.space import SpaceWrapper
 
 eta = 3
 
 
-def schedule(dataset, sh: BudgetAwareControllerSH, T_,
-             t1_, t2_,
-             w_,
-             search_space_ins, N_K_ratio, only_phase1: bool = False):
-    """
-    dataset: dataset name
-    sh: sh
-    T_: total time budget
-    t1_: time for score a model
-    t2_: time for train a model
-    w_:  number of workers, only used in distributed EA
-    space_name: search_space instance, 101, 201 MLP
-    N_K_ratio: ratio
-    only_phase1: if only using phase1.
-    """
-    if T_ < 1:
-        raise
+def min_budget_calculation(search_space_ins: SpaceWrapper, dataset: str,
+                           N_K_ratio: int, sh: BudgetAwareControllerSH, t1_: float):
+    # Calculate the minimum budget requirements for both phases
+    K_max = int(len(search_space_ins) / N_K_ratio)
 
-    # min time budget used for having both phases, when K = 1, N = min_budget_required_both_phase * 1
     if search_space_ins.name == Config.NB101:
-        K_max = int(len(search_space_ins) / N_K_ratio)
         U_options = [4, 12, 16, 108]
-        U_min = U_options[0]
-        min_budget_required_both_phase = sh.pre_calculate_time_required(K=1, U=U_min)[1] + N_K_ratio * t1_
     elif search_space_ins.name == Config.NB201:
-        K_max = int(len(search_space_ins) / N_K_ratio)
         U_options = list(range(1, 200))
-        U_min = U_options[0]
-        min_budget_required_both_phase = sh.pre_calculate_time_required(K=1, U=U_min)[1] + N_K_ratio * t1_
     elif search_space_ins.name == Config.MLPSP:
-        K_max = int(len(search_space_ins) / N_K_ratio)
-        # todo: this is for benchmark only
+        # TODO: This is for benchmark only
         if dataset == Config.Frappe:
             MaxEpochTrained = 20
         elif dataset == Config.Criteo:
@@ -46,54 +26,73 @@ def schedule(dataset, sh: BudgetAwareControllerSH, T_,
         else:
             raise NotImplementedError
         U_options = list(range(1, MaxEpochTrained))
-
-        U_min = U_options[0]
-        min_budget_required_both_phase = sh.pre_calculate_time_required(K=1, U=U_min)[1] + N_K_ratio * t1_
     else:
         raise NotImplementedError
 
+    U_min = U_options[0]
+    min_budget_required_both_phase = sh.pre_calculate_time_required(K=1, U=U_min)[1] + N_K_ratio * t1_
+
+    return K_max, U_options, U_min, min_budget_required_both_phase
+
+
+def schedule(dataset: str, sh: BudgetAwareControllerSH, T_: float, t1_: float, t2_: float, w_: int,
+             search_space_ins: SpaceWrapper, N_K_ratio: int,
+             only_phase1: bool = False):
+    """
+    :param dataset
+    :param sh: BudgetAwareControllerSH instnace
+    :param T_: user given time budget
+    :param t1_: time to score one model
+    :param t2_: time to train one model
+    :param w_: number of workers, for parallelly running.
+    :param search_space_ins: search spcae instance
+    :param N_K_ratio: N/K = N_K_ratio
+    :param only_phase1: Only use filtering phase.
+    """
+    if T_ < 1:
+        raise ValueError('Total time budget must be greater than 1 second')
+
+    K_max, U_options, U_min, min_budget_required_both_phase = min_budget_calculation(
+        search_space_ins, dataset, N_K_ratio, sh, t1_)
+
+    # collection of (best_K, best_U, best_N)
     history = []
 
-    # if there is only phase 1
-    time_used = 1 * t1_
+    # Calculate phase 1
+    time_used = t1_
     enable_phase2_at_least = sh.pre_calculate_time_required(K=2, U=U_min)[1] + 2 * N_K_ratio * t1_
+
     if only_phase1 or enable_phase2_at_least > T_:
-        # all time give to phase1
-        for N_only in range(1, min(int(T_ / t1_), len(search_space_ins)) + 1):
-            time_used = N_only * t1_
-            if time_used > T_:
-                break
-            else:
-                history.append((1, U_min, N_only))
+        # all time give to phase1, explore n models
+        N_only = min(int(T_ / t1_), len(search_space_ins))
+        history.extend([(1, U_min, i) for i in range(1, N_only + 1) if i * t1_ <= T_])
+        if not history:
+            raise ValueError(
+                f' [trails] Only p1, Budget {T_} is too small, it\'s at least >= {time_used} with current worker, '
+                f'{t1_}, {t2_}, eta')
 
-        if len(history) == 0:
-            logger.info(
-                f" [trails] Only p1, Budget {T_} is too small, it's at least >= {time_used} "
-                f"with current worker, {t1_}, {t2_}, eta")
-            raise
-
+    # Calculate phase 2
     else:
         # record all possible K, U pair meeting the SLO ( time used < T)
         for K_ in range(2, min(int(T_ / t1_), K_max) + 1):
             N_ = K_ * N_K_ratio
             for U in U_options:
-                # when K ==1, phase 2 is zero
                 time_used = sh.pre_calculate_time_required(K=K_, U=U)[1] + N_ * t1_
                 if time_used > T_:
                     break
                 else:
                     history.append((K_, U, N_))
+        if not history:
+            raise ValueError(
+                f' [trails] Budget {T_} is too small, it\'s at least >= {min_budget_required_both_phase}'
+                f' with current worker, {t1_}, {t2_}, eta')
 
-        if len(history) == 0:
-            logger.info(
-                f" [trails] Budget {T_} is too small, it's at least >= {min_budget_required_both_phase} "
-                f"with current worker, {t1_}, {t2_}, eta")
-            raise
-
-    best_K, best_U, best_N = history[-1][0], history[-1][1], history[-1][2]
+    best_K, best_U, best_N = history[-1]
     N_scored = best_N
     B1_time_used = N_scored * t1_
     B2_all_epoch, B2_time_used = sh.pre_calculate_time_required(K=best_K, U=best_U)
+
     logger.info(
-        f" [trails] The schedule result: when T = {T_} second, N = {N_scored}, K = {best_K}, best_U = {best_U}, time_used = {B1_time_used + B2_time_used}")
+        f' [trails] The schedule result: when T = {T_} second, N = {N_scored}, K = {best_K}, best_U = {best_U}, '
+        f'time_used = {B1_time_used + B2_time_used}')
     return best_K, best_U, N_scored, B1_time_used, B2_time_used, B2_all_epoch
