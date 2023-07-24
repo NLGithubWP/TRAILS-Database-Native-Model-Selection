57b1df98a9cb3622fad1509d837072d85603f190
working <57171759+NLGithubWP@users.noreply.github.com>
2023-07-24 11:37:36 +0800

    remove un-used folder

--- z_dev/sync.sh
+++
@@ -1,9 +0,0 @@
-
-
-sshpass -p "xingnaili" scp ./TRAILS/Dockerfile xingnaili@panda.d2.comp.nus.edu.sg:/home/xingnaili/firmest_docker/TRAILS
-sshpass -p "xingnaili" scp -r ./TRAILS/internal/pg_extension xingnaili@panda.d2.comp.nus.edu.sg:/home/xingnaili/firmest_docker/TRAILS/internal
-sshpass -p "xingnaili" scp -r ./TRAILS/internal/ml/model_selec xingnaili@panda.d2.comp.nus.edu.sg:/home/xingnaili/firmest_docker/TRAILS/internal/ml/
-#sshpass -p "xingnaili" scp -r ./TRAILS/internal xingnaili@panda.d2.comp.nus.edu.sg:/home/xingnaili/firmest_docker/TRAILS/
-
-
-#sshpass -p "xingnaili" scp -r ./TRAILS/internal/pg_extension/src/lib.rs xingnaili@panda.d2.comp.nus.edu.sg:/home/xingnaili/firmest_docker/TRAILS/internal/pg_extension/src
