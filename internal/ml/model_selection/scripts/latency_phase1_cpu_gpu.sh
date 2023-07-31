c0b2bb5aaa7fff051b0c09234d280c45715b011c
working <57171759+NLGithubWP@users.noreply.github.com>
2023-07-31 14:24:06 +0800

    Draw both cpu and gpu

--- internal/ml/model_selection/scripts/latency_phase1_cpu_gpu.sh
+++ internal/ml/model_selection/scripts/latency_phase1_cpu_gpu.sh
@@ -110,4 +110,113 @@ python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py 
   --result_dir=./internal/ml/model_selection/exp_result/ \
   --log_folder=log_score_time_frappe
 
+########################## CPU ##############################
+# this is run on cpu, only change the device==cpu for all above
 
+# frappe
+python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py \
+  --tfmem=express_flow \
+  --models_explore=5000 \
+  --log_name=score_based \
+  --search_space=mlp_sp \
+  --num_layers=4 \
+  --hidden_choice_len=20 \
+  --base_dir=/hdd1/xingnaili/exp_data/ \
+  --num_labels=2 \
+  --device=cpu \
+  --batch_size=32 \
+  --dataset=frappe \
+  --nfeat=5500 \
+  --nfield=10 \
+  --nemb=10 \
+  --workers=0 \
+  --result_dir=./internal/ml/model_selection/exp_result/ \
+  --log_folder=log_score_time_frappe
+
+#criteo
+python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py \
+  --tfmem=express_flow \
+  --models_explore=5000 \
+  --log_name=score_based \
+  --search_space=mlp_sp \
+  --num_layers=4 \
+  --hidden_choice_len=10 \
+  --base_dir=/hdd1/xingnaili/exp_data/ \
+  --num_labels=2 \
+  --device=cpu \
+  --batch_size=32 \
+  --dataset=criteo \
+  --nfeat=2100000 \
+  --nfield=39 \
+  --nemb=10 \
+  --workers=0 \
+  --result_dir=./internal/ml/model_selection/exp_result/ \
+  --log_folder=log_score_time_frappe
+
+# uci
+python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py \
+  --tfmem=express_flow \
+  --models_explore=5000 \
+  --log_name=score_based \
+  --search_space=mlp_sp \
+  --num_layers=4 \
+  --hidden_choice_len=20 \
+  --base_dir=/hdd1/xingnaili/exp_data/ \
+  --num_labels=2 \
+  --device=cpu \
+  --batch_size=32 \
+  --dataset=uci_diabetes \
+  --nfeat=369 \
+  --nfield=43 \
+  --nemb=10 \
+  --workers=0 \
+  --result_dir=./internal/ml/model_selection/exp_result/ \
+  --log_folder=log_score_time_frappe
+
+
+# cifar 10
+python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py \
+  --tfmem=synflow \
+  --models_explore=5000 \
+  --log_name=score_based \
+  --search_space=nasbench201 \
+  --api_loc=NAS-Bench-201-v1_1-096897.pth \
+  --base_dir=/hdd1/xingnaili/exp_data/ \
+  --num_labels=10 \
+  --device=cpu \
+  --batch_size=32 \
+  --dataset=cifar10 \
+  --result_dir=./internal/ml/model_selection/exp_result/ \
+  --log_folder=log_score_time_frappe
+
+
+# cifar 100
+python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py \
+  --tfmem=synflow \
+  --models_explore=5000 \
+  --log_name=score_based \
+  --search_space=nasbench201 \
+  --api_loc=NAS-Bench-201-v1_1-096897.pth \
+  --base_dir=/hdd1/xingnaili/exp_data/ \
+  --num_labels=100 \
+  --device=cpu \
+  --batch_size=32 \
+  --dataset=cifar100 \
+  --result_dir=./internal/ml/model_selection/exp_result/ \
+  --log_folder=log_score_time_frappe
+
+
+# imageNet
+python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py \
+  --tfmem=synflow \
+  --models_explore=5000 \
+  --log_name=score_based \
+  --search_space=nasbench201 \
+  --api_loc=NAS-Bench-201-v1_1-096897.pth \
+  --base_dir=/hdd1/xingnaili/exp_data/ \
+  --num_labels=120 \
+  --device=cpu \
+  --batch_size=32 \
+  --dataset=ImageNet16-120 \
+  --result_dir=./internal/ml/model_selection/exp_result/ \
+  --log_folder=log_score_time_frappe
