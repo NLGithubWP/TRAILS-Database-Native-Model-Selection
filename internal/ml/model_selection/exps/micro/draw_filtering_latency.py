17eab52c0b4f2640b1db5118f7bd6c31543e9b5e
working <57171759+NLGithubWP@users.noreply.github.com>
2023-07-31 20:42:25 +0800

    Add datasets time measure draw graph

--- internal/ml/model_selection/exps/micro/draw_filtering_latency.py
+++ internal/ml/model_selection/exps/micro/draw_filtering_latency.py
@@ -5,42 +5,44 @@ import numpy as np
 # Assume these are the names and corresponding JSON files of your datasets
 datasets = {
     'frappe': {'cpu': './internal/ml/model_selection/exp_result_sever/exp_result'
-                      '/time_score_mlp_sp_frappe_batch_size_32_cuda:0.json',
+                      '/time_score_mlp_sp_frappe_batch_size_32_cpu.json',
                'gpu': './internal/ml/model_selection/exp_result_sever/exp_result'
                       '/time_score_mlp_sp_frappe_batch_size_32_cuda:0.json'},
 
     'diabetes': {'cpu': './internal/ml/model_selection/exp_result_sever/exp_result'
-                        '/time_score_mlp_sp_frappe_batch_size_32_cuda:0.json',
+                        '/time_score_mlp_sp_uci_diabetes_batch_size_32_cpu.json',
                  'gpu': './internal/ml/model_selection/exp_result_sever/exp_result'
-                        '/time_score_mlp_sp_frappe_batch_size_32_cuda:0.json'},
+                        '/time_score_mlp_sp_uci_diabetes_batch_size_32_cuda:0.json'},
 
     'criteo': {'cpu': './internal/ml/model_selection/exp_result_sever/exp_result'
-                      '/time_score_mlp_sp_frappe_batch_size_32_cuda:0.json',
+                      '/time_score_mlp_sp_criteo_batch_size_32_cpu.json',
                'gpu': './internal/ml/model_selection/exp_result_sever/exp_result'
                       '/time_score_mlp_sp_criteo_batch_size_32_cuda:0.json'},
 
-    'cifar10': {'cpu': './internal/ml/model_selection/exp_result_sever/exp_result'
-                       '/time_score_mlp_sp_frappe_batch_size_32_cuda:0.json',
-                'gpu': './internal/ml/model_selection/exp_result_sever/exp_result'
-                       '/time_score_nasbench201_cifar10_batch_size_32_cuda:0.json'},
+    'c10': {'cpu': './internal/ml/model_selection/exp_result_sever/exp_result'
+                   '/time_score_nasbench201_cifar10_batch_size_32_cpu.json',
+            'gpu': './internal/ml/model_selection/exp_result_sever/exp_result'
+                   '/time_score_nasbench201_cifar10_batch_size_32_cuda:0.json'},
 
-    'cifar100': {'cpu': './internal/ml/model_selection/exp_result_sever/exp_result'
-                        '/time_score_mlp_sp_frappe_batch_size_32_cuda:0.json',
-                 'gpu': './internal/ml/model_selection/exp_result_sever/exp_result'
-                        '/time_score_nasbench201_cifar100_batch_size_32_cuda:0.json'},
+    'c100': {'cpu': './internal/ml/model_selection/exp_result_sever/exp_result'
+                    '/time_score_nasbench201_cifar100_batch_size_32_cpu.json',
+             'gpu': './internal/ml/model_selection/exp_result_sever/exp_result'
+                    '/time_score_nasbench201_cifar100_batch_size_32_cuda:0.json'},
 
-    'ImageNet16-120': {'cpu': './internal/ml/model_selection/exp_result_sever/exp_result'
-                              '/time_score_mlp_sp_frappe_batch_size_32_cuda:0.json',
-                       'gpu': './internal/ml/model_selection/exp_result_sever/exp_result'
-                              '/time_score_nasbench201_ImageNet16-120_batch_size_32_cuda:0.json'},
+    'IN-16': {'cpu': './internal/ml/model_selection/exp_result_sever/exp_result'
+                     '/time_score_nasbench201_ImageNet16-120_batch_size_32_cpu.json',
+              'gpu': './internal/ml/model_selection/exp_result_sever/exp_result'
+                     '/time_score_nasbench201_ImageNet16-120_batch_size_32_cuda:0.json'},
 }
 
 # Set your plot parameters
-bar_width = 0.15
+bar_width = 0.25
 opacity = 0.8
 fontsize = 14  # Set the font size
-colors = ['#729ECE', '#FFB579', '#98DF8A']  # Softer colors #FF7F7F
+cpu_colors = ['#729ECE', '#FFB579']  # Colors for CPU bars
+gpu_colors = ['#98DF8A', '#D62728']  # Colors for GPU bars
 hatches = ['/', '\\', 'x', '.', '*']
+# hatches = ['', '', '', '', '']
 
 # Create the figure and axis
 fig, ax = plt.subplots(figsize=(6.4, 4.5))
@@ -57,21 +59,21 @@ for i, (dataset_name, json_files) in enumerate(datasets.items()):
 
     # Plot bars for cpu
     ax.bar(i - bar_width / 2, data_cpu['io_latency'], bar_width,
-           alpha=opacity, color=colors[0], hatch=hatches[0], edgecolor='black',
+           alpha=opacity, color=cpu_colors[0], hatch=hatches[0], edgecolor='black',
            label='CPU IO' if i == 0 else "")
 
     ax.bar(i - bar_width / 2, data_cpu['compute_latency'], bar_width,
-           alpha=opacity, color=colors[1], hatch=hatches[1], edgecolor='black',
+           alpha=opacity, color=cpu_colors[1], hatch=hatches[1], edgecolor='black',
            label='CPU Compute' if i == 0 else "",
            bottom=data_cpu['io_latency'])
 
     # Plot bars for gpu
     ax.bar(i + bar_width / 2, data_gpu['io_latency'], bar_width,
-           alpha=opacity, color=colors[0], hatch=hatches[2], edgecolor='black',
+           alpha=opacity, color=gpu_colors[0], hatch=hatches[2], edgecolor='black',
            label='GPU IO' if i == 0 else "")
 
     ax.bar(i + bar_width / 2, data_gpu['compute_latency'], bar_width,
-           alpha=opacity, color=colors[1], hatch=hatches[3], edgecolor='black',
+           alpha=opacity, color=gpu_colors[1], hatch=hatches[3], edgecolor='black',
            label='GPU Compute' if i == 0 else "",
            bottom=data_gpu['io_latency'])
 
@@ -80,19 +82,18 @@ ax.set_xticks(range(len(datasets)))
 ax.set_xticklabels(datasets.keys(), fontsize=fontsize)
 
 # Set axis labels
-ax.set_ylabel('Time Consumption (s)', fontsize=fontsize)
+ax.set_ylabel('Latency (s)', fontsize=fontsize)
 
 # Set y-scale to logarithmic
 ax.set_yscale('log')
 # adjust the upper limit as per your need
-plt.ylim(0, 10**5)
+plt.ylim(0, 20 ** 3)
 
 # Set other parameters and show plot
-ax.legend(fontsize=fontsize, loc='upper right')
-ax.legend(loc='upper right', ncol=2, fontsize=fontsize)
+ax.legend(fontsize=fontsize, loc='upper right', ncol=2)
 fig.tight_layout()
 plt.show()
 
 # Save the plot
-fig.savefig("./internal/ml/model_selection/exp_result_sever/exp_result/image.pdf",
+fig.savefig("./internal/ml/model_selection/exp_result_sever/exp_result/filter_latency.pdf",
             bbox_inches='tight')
