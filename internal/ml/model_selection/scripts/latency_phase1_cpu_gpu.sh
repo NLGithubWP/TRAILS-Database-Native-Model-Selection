a534d9d6dc88fe662b47a89a9a243dc7b8db1a55
working <57171759+NLGithubWP@users.noreply.github.com>
2023-07-31 13:57:55 +0800

    Add more scripts to measure ltanecy

--- internal/ml/model_selection/scripts/latency_phase1_cpu_gpu.sh
+++ internal/ml/model_selection/scripts/latency_phase1_cpu_gpu.sh
@@ -3,7 +3,7 @@
 export PYTHONPATH=$PYTHONPATH:./internal/ml/model_selection
 
 # frappe
-nohup  python3 ./internal/ml/model_selection/exps/nas_bench_tabular/benchmark_filtering_latency.py \
+nohup  python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py \
   --tfmem=express_flow \
   --models_explore=10 \
   --log_name=score_based \
@@ -23,7 +23,7 @@ nohup  python3 ./internal/ml/model_selection/exps/nas_bench_tabular/benchmark_fi
   --log_folder=log_score_time_frappe  > output.log&
 
 #critep
-nohup  python3 ./internal/ml/model_selection/exps/nas_bench_tabular/benchmark_filtering_latency.py \
+nohup  python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py \
   --tfmem=express_flow \
   --models_explore=10 \
   --log_name=score_based \
@@ -43,7 +43,7 @@ nohup  python3 ./internal/ml/model_selection/exps/nas_bench_tabular/benchmark_fi
   --log_folder=log_score_time_frappe  > output.log&
 
 # uci
-nohup  python3 ./internal/ml/model_selection/exps/nas_bench_tabular/benchmark_filtering_latency.py \
+nohup  python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py \
   --tfmem=express_flow \
   --models_explore=10 \
   --log_name=score_based \
@@ -61,4 +61,53 @@ nohup  python3 ./internal/ml/model_selection/exps/nas_bench_tabular/benchmark_fi
   --workers=0 \
   --result_dir=./internal/ml/model_selection/exp_result/ \
   --log_folder=log_score_time_frappe  > output.log&
+
+
+# cifar 10
+nohup  python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py \
+  --tfmem=synflow \
+  --models_explore=10 \
+  --log_name=score_based \
+  --search_space=nasbench201 \
+  --api_loc=NAS-Bench-201-v1_1-096897.pth \
+  --base_dir=/hdd1/xingnaili/exp_data/ \
+  --num_labels=10 \
+  --device=cuda:0 \
+  --batch_size=32 \
+  --dataset=cifar10 \
+  --result_dir=./internal/ml/model_selection/exp_result/ \
+  --log_folder=log_score_time_frappe  > output.log&
+
+
+# cifar 100
+nohup  python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py \
+  --tfmem=synflow \
+  --models_explore=10 \
+  --log_name=score_based \
+  --search_space=nasbench201 \
+  --api_loc=NAS-Bench-201-v1_1-096897.pth \
+  --base_dir=/hdd1/xingnaili/exp_data/ \
+  --num_labels=100 \
+  --device=cuda:0 \
+  --batch_size=32 \
+  --dataset=cifar100 \
+  --result_dir=./internal/ml/model_selection/exp_result/ \
+  --log_folder=log_score_time_frappe  > output.log&
+
+
+# imageNet
+nohup  python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py \
+  --tfmem=synflow \
+  --models_explore=10 \
+  --log_name=score_based \
+  --search_space=nasbench201 \
+  --api_loc=NAS-Bench-201-v1_1-096897.pth \
+  --base_dir=/hdd1/xingnaili/exp_data/ \
+  --num_labels=120 \
+  --device=cuda:0 \
+  --batch_size=32 \
+  --dataset=ImageNet16-120 \
+  --result_dir=./internal/ml/model_selection/exp_result/ \
+  --log_folder=log_score_time_frappe  > output.log&
+
 
