0879a6500d936426c0ef7bcfcd84366dea1365ab
working <57171759+NLGithubWP@users.noreply.github.com>
2023-08-01 17:48:26 +0800

    Capture main process and children process's resource usage

--- internal/ml/model_selection/src/tools/res_measure.py
+++ internal/ml/model_selection/src/tools/res_measure.py
@@ -9,23 +9,20 @@ from src.tools.io_tools import write_json
 def print_cpu_gpu_usage(interval=1, output_file="path_to_folder", stop_event=None):
     def print_usage():
         # Get current process
-        process = psutil.Process(os.getpid())
+        main_process = psutil.Process(os.getpid())
 
         # Create an empty dictionary to store metrics
         metrics = {'cpu_usage': [], 'memory_usage': [], 'gpu_usage': []}
 
         while not stop_event.is_set():
-            # Get CPU usage for current process
-            cpu_percent = process.cpu_percent()
-            metrics['cpu_usage'].append(cpu_percent)
-            # print(f"CPU usage: {cpu_percent}%")
+            cpu_percent = 0
+            mem_usage_mb = 0
+            for process in main_process.children(recursive=True):  # Include all child processes
+                cpu_percent += process.cpu_percent()
+                mem_usage_mb += process.memory_info().rss / (1024 ** 2)
 
-            # Get memory usage for current process
-            memory_info = process.memory_info()
-            # bytes -> MB
-            mem_usage_mb = memory_info.rss / (1024 ** 2)
+            metrics['cpu_usage'].append(cpu_percent)
             metrics['memory_usage'].append(mem_usage_mb)
-            # print(f"Memory usage: {mem_usage_mb}MB")
 
             # Get GPU usage
             gpu_stats = gpustat.GPUStatCollection.new_query()
