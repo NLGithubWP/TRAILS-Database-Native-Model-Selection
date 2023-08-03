aa38480b15e7bd0a8a0c69a20403eb701ebbb7e9
working <57171759+NLGithubWP@users.noreply.github.com>
2023-08-03 20:44:32 +0800

    Update the score scripts

--- internal/ml/model_selection/scripts/nas-bench-tabular/score_all_modesl_uci.sh
+++ internal/ml/model_selection/scripts/nas-bench-tabular/score_all_modesl_uci.sh
@@ -5,11 +5,11 @@ export PYTHONPATH=$PYTHONPATH:./internal/ml/model_selection
 nohup  python ./internal/ml/model_selection/exps/nas_bench_tabular/4.seq_score_online.py \
   --models_explore=159999 \
   --tfmem=express_flow \
-  --log_name=score_based_all_metrics \
+  --log_name=score_based \
   --search_space=mlp_sp \
   --num_layers=4 \
   --hidden_choice_len=20 \
-  --base_dir=../exp_data/ \
+  --base_dir=/hdd1/xingnaili/exp_data/ \
   --num_labels=2 \
   --device=cuda:2 \
   --batch_size=32 \
