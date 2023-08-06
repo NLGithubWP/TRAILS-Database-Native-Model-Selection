cca143599ba354c7522f5cace3d861ba4c8c7efe
working <57171759+NLGithubWP@users.noreply.github.com>
2023-08-07 03:44:17 +0800

    Add combinations

--- internal/ml/model_selection/exps/micro/resp/benchmark_combinations.py
+++ internal/ml/model_selection/exps/micro/resp/benchmark_combinations.py
@@ -4,6 +4,18 @@ import calendar
 import os
 import time
 import matplotlib
+
+args = parse_arguments()
+
+gmt = time.gmtime()
+ts = calendar.timegm(gmt)
+os.environ.setdefault("log_logger_folder_name", f"{args.log_folder}")
+os.environ.setdefault("log_file_name", args.log_name + "_" + str(ts) + ".log")
+os.environ.setdefault("base_dir", args.base_dir)
+from src.query_api.interface import SimulateScore
+from src.eva_engine.phase2.evaluator import P2Evaluator
+from src.search_space.init_search_space import init_search_space
+
 matplotlib.use('TkAgg')  # Or any other supported backend
 from matplotlib import pyplot as plt
 import numpy as np
@@ -68,7 +80,7 @@ def run_evolution_search(max_trained_models=1000,
         pool.append((full_train_auc, arch_micro))
 
         cur_time_used += time_usage
-        time_usage_lst.append(cur_time_used)
+        time_usage_lst.append(cur_time_used / 60)
 
         if full_train_auc > best_tests[-1]:
             best_tests.append(full_train_auc)
@@ -108,7 +120,7 @@ def run_evolution_search(max_trained_models=1000,
         num_trained_models += 1
 
         cur_time_used += time_usage
-        time_usage_lst.append(cur_time_used)
+        time_usage_lst.append(cur_time_used / 60)
 
         # kill the oldest individual in the population.
         pool.append((full_train_auc, arch_micro))
@@ -121,7 +133,7 @@ def run_evolution_search(max_trained_models=1000,
 
         if num_trained_models >= max_trained_models:
             break
-
+    # return time in mins
     return time_usage_lst, best_tests
 
 
@@ -135,40 +147,79 @@ def plot_experiment(exp_list, title):
         q_75_y = np.quantile(exp, .75, axis=0)
         q_25_y = np.quantile(exp, .25, axis=0)
         mean_y = np.mean(exp, axis=0)
-
         time_usg = np.array(time_usg)
         q_75_time = np.quantile(time_usg, .75, axis=0)
         q_25_time = np.quantile(time_usg, .25, axis=0)
         mean_time = np.mean(time_usg, axis=0)
-
         plt.plot(mean_time, mean_y, label=label)
         plt.fill_between(mean_time, q_25_y, q_75_y, alpha=0.1)
+
+        # exp = np.array(exp)
+        # q_75 = np.quantile(exp, .75, axis=0)
+        # q_25 = np.quantile(exp, .25, axis=0)
+        # mean = np.mean(exp, axis=0)
+        # plt.plot(mean, label=label)
+        # plt.fill_between(range(len(q_25)), q_25, q_75, alpha=0.1)
+
+    fig, ax = plt.subplots()
 
     for time_usg, exp, ename in exp_list:
         plot_exp(time_usg, exp, ename)
     plt.grid()
-    plt.xlabel('Trained Models')
+    plt.xlabel('Time in mins')
     plt.ylabel('Test Accuracy')
-    # plt.ylim(70, 73.6)
+    plt.ylim(0.9700, 0.9815)
 
-    plt.xscale('xlog')
+    plt.xscale("log")
+    plt.xlim([0.01, 150])
 
     plt.legend()
     plt.title(title)
-    plt.show()
+    fig.savefig(f"./internal/ml/model_selection/exp_result/warm_up_move_proposal.pdf",
+                bbox_inches='tight')
+
+
+def export_warm_up_move_proposal(tfmem: str):
+
+    total_run = 5
+    ae_warmup_time, ae_warmup = [], []
+    for run_id in range(total_run):
+        _ae_warmup_time, _ae_warmup = run_evolution_search(
+            max_trained_models=100,
+            pool_size=64,
+            tournament_size=10,
+            zero_cost_warmup=3000,
+            zero_cost_move=False,
+            tfmem=tfmem,
+            dataset_name="frappe",
+            query_epoch=19,
+            args=args)
+
+        ae_warmup_time.append(_ae_warmup_time)
+        ae_warmup.append(_ae_warmup)
+
+    ae_move_time, ae_move = [], []
+    for run_id in range(total_run):
+        _ae_move_time, _ae_move = run_evolution_search(
+            max_trained_models=100,
+            pool_size=64,
+            tournament_size=10,
+            zero_cost_warmup=0,
+            zero_cost_move=True,
+            tfmem=tfmem,
+            dataset_name="frappe",
+            query_epoch=19,
+            args=args)
+
+        ae_move_time.append(_ae_move_time)
+        ae_move.append(_ae_move)
+
+    return [ae_warmup_time, ae_warmup, f"EA + warmup-{tfmem} (3K)"], \
+           [ae_move_time, ae_move, f"EA + move-{tfmem}"]
 
 
 if __name__ == "__main__":
-    args = parse_arguments()
 
-    gmt = time.gmtime()
-    ts = calendar.timegm(gmt)
-    os.environ.setdefault("log_logger_folder_name", f"{args.log_folder}")
-    os.environ.setdefault("log_file_name", args.log_name + "_" + str(ts) + ".log")
-    os.environ.setdefault("base_dir", args.base_dir)
-    from src.query_api.interface import SimulateScore
-    from src.eva_engine.phase2.evaluator import P2Evaluator
-    from src.search_space.init_search_space import init_search_space
 
     total_run = 5
 
@@ -176,7 +227,7 @@ if __name__ == "__main__":
 
     for run_id in range(total_run):
         _ae_warmup_time, _ae_warmup = run_evolution_search(
-            max_trained_models=1000,
+            max_trained_models=100,
             pool_size=64,
             tournament_size=10,
             zero_cost_warmup=3000,
@@ -192,7 +243,7 @@ if __name__ == "__main__":
     ae_move_time, ae_move = [], []
     for run_id in range(total_run):
         _ae_move_time, _ae_move = run_evolution_search(
-            max_trained_models=1000,
+            max_trained_models=100,
             pool_size=64,
             tournament_size=10,
             zero_cost_warmup=0,
