fd62d8abef5e3f030501f0ba78f019ee65a69f7b
working <57171759+NLGithubWP@users.noreply.github.com>
2023-08-07 00:20:47 +0800

    Add ntk cond and grasp

--- internal/ml/model_selection/exps/micro/resp/search_cost_draw.py
+++ internal/ml/model_selection/exps/micro/resp/search_cost_draw.py
@@ -63,8 +63,8 @@ def find_time_for_target_accuracy(elements, target_accuracy_index, target_algo='
     # Find target accuracy for the algorithm
     for e in elements:
         x, y, algo = e
-        x = [val[1:] for val in x]  # Skip the first element in each list of x
-        y = [val[1:] for val in y]  # Skip the first element in each list of x
+        # x = [val[1:] for val in x]  # Skip the first element in each list of x
+        # y = [val[1:] for val in y]  # Skip the first element in each list of x
         x = [np.median(val) for val in zip(*x)]  # Calculate median per column
         y = [np.median(val) for val in zip(*y)]  # Calculate median per column
         if algo == target_algo:
@@ -78,8 +78,8 @@ def find_time_for_target_accuracy(elements, target_accuracy_index, target_algo='
     # Interpolate for other algorithms
     for e in elements:
         x, y, algo = e
-        x = [val[1:] for val in x]  # Skip the first element in each list of x
-        y = [val[1:] for val in y]  # Skip the first element in each list of x
+        # x = [val[1:] for val in x]  # Skip the first element in each list of x
+        # y = [val[1:] for val in y]  # Skip the first element in each list of x
         x = [np.median(val) for val in zip(*x)]  # Calculate median per column
         y = [np.median(val) for val in zip(*y)]  # Calculate median per column
         if target_accuracy > max(y):
@@ -111,7 +111,7 @@ def generate_and_draw_data(dataset):
 
         all_lines.append([sampled_x, sampled_y, key])
 
-    pprint(find_time_for_target_accuracy(elements=all_lines, target_accuracy_index=2))
+    pprint(find_time_for_target_accuracy(elements=all_lines, target_accuracy_index=3))
 
     draw_structure_data_anytime(
         all_lines=all_lines,
