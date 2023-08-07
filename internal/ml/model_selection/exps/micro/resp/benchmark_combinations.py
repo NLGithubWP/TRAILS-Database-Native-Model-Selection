8d8d23e9720b69fca6036d21f8b1f2f299c2c18f
working <57171759+NLGithubWP@users.noreply.github.com>
2023-08-07 12:04:23 +0800

    Add filtering-refinmeent with fully training.

--- internal/ml/model_selection/exps/micro/resp/benchmark_combinations.py
+++ internal/ml/model_selection/exps/micro/resp/benchmark_combinations.py
@@ -15,6 +15,8 @@ os.environ.setdefault("base_dir", args.base_dir)
 from src.query_api.interface import SimulateScore
 from src.eva_engine.phase2.evaluator import P2Evaluator
 from src.search_space.init_search_space import init_search_space
+from src.eva_engine.run_ms import RunModelSelection
+from src.query_api.interface import profile_NK_trade_off
 
 matplotlib.use('TkAgg')  # Or any other supported backend
 from matplotlib import pyplot as plt
@@ -137,50 +139,54 @@ def run_evolution_search(max_trained_models=1000,
     return time_usage_lst, best_tests
 
 
-def filter_refinment_fully_training():
-    pass
+def filter_refinment_fully_training(dataset_name="frappe",
+                                    query_epoch=19):
 
+    print("Running filter_refinment_fully_training...")
 
-def plot_experiment(exp_list, title):
-    def plot_exp(time_usg, exp, label):
-        exp = np.array(exp)
-        q_75_y = np.quantile(exp, .75, axis=0)
-        q_25_y = np.quantile(exp, .25, axis=0)
-        mean_y = np.mean(exp, axis=0)
-        time_usg = np.array(time_usg)
-        q_75_time = np.quantile(time_usg, .75, axis=0)
-        q_25_time = np.quantile(time_usg, .25, axis=0)
-        mean_time = np.mean(time_usg, axis=0)
-        plt.plot(mean_time, mean_y, label=label)
-        plt.fill_between(mean_time, q_25_y, q_75_y, alpha=0.1)
+    rms = RunModelSelection(args.search_space, args, is_simulate=True)
+    search_space_ins = init_search_space(args)
+    _evaluator = P2Evaluator(search_space_ins, dataset_name,
+                             is_simulate=True,
+                             train_loader=None,
+                             val_loader=None)
 
-        # exp = np.array(exp)
-        # q_75 = np.quantile(exp, .75, axis=0)
-        # q_25 = np.quantile(exp, .25, axis=0)
-        # mean = np.mean(exp, axis=0)
-        # plt.plot(mean, label=label)
-        # plt.fill_between(range(len(q_25)), q_25, q_75, alpha=0.1)
+    score_time_per_model = rms.profile_filtering()
+    n_k_ratio = profile_NK_trade_off(dataset_name)
 
-    fig, ax = plt.subplots()
+    total_run = 5
+    auc_lst = []
+    time_usage_lst = []
+    for run_id in range(total_run):
+        _time_usage_lst = []
+        _auc_lst = []
+        for N in range(30, 3030, 50):
+            cur_time_used = 0
+            K = int(N/n_k_ratio)
+            k_models, all_models, p1_trace_highest_score, p1_trace_highest_scored_models_id = rms.filtering_phase(
+                N=N, K=K)
+            cur_time_used += N*score_time_per_model
+            if K > 1:
+                train_auc = []
+                for arch_id in k_models:
+                    full_train_auc, time_usage = _evaluator.p2_evaluate(str(arch_id), query_epoch)
+                    cur_time_used += time_usage
+                    train_auc.append(full_train_auc)
+            else:
+                train_auc = []
+                full_train_auc, time_usage = _evaluator.p2_evaluate(str(k_models[0]), query_epoch)
+                train_auc.append(full_train_auc)
 
-    for time_usg, exp, ename in exp_list:
-        plot_exp(time_usg, exp, ename)
-    plt.grid()
-    plt.xlabel('Time in mins')
-    plt.ylabel('Test Accuracy')
-    plt.ylim(0.9700, 0.9815)
+            _auc_lst.append(max(train_auc))
+            _time_usage_lst.append(cur_time_used / 60)
+        auc_lst.append(_auc_lst)
+        time_usage_lst.append(_time_usage_lst)
 
-    plt.xscale("log")
-    plt.xlim([0.01, 150])
-
-    plt.legend()
-    plt.title(title)
-    fig.savefig(f"./internal/ml/model_selection/exp_result/warm_up_move_proposal.pdf",
-                bbox_inches='tight')
+    return [time_usage_lst, auc_lst, "Filtering + Refinement (Fully Train)"]
 
 
 def export_warm_up_move_proposal(tfmem: str):
-
+    print("Running warm_up_move_proposal...")
     total_run = 5
     ae_warmup_time, ae_warmup = [], []
     for run_id in range(total_run):
@@ -218,46 +224,55 @@ def export_warm_up_move_proposal(tfmem: str):
            [ae_move_time, ae_move, f"EA + move-{tfmem}"]
 
 
+def plot_experiment(exp_list, title):
+    def plot_exp(time_usg, exp, label):
+        exp = np.array(exp)
+        q_75_y = np.quantile(exp, .75, axis=0)
+        q_25_y = np.quantile(exp, .25, axis=0)
+        mean_y = np.mean(exp, axis=0)
+        time_usg = np.array(time_usg)
+        q_75_time = np.quantile(time_usg, .75, axis=0)
+        q_25_time = np.quantile(time_usg, .25, axis=0)
+        mean_time = np.mean(time_usg, axis=0)
+        plt.plot(mean_time, mean_y, "-*", label=label, )
+        plt.fill_between(mean_time, q_25_y, q_75_y, alpha=0.1)
+
+        # exp = np.array(exp)
+        # q_75 = np.quantile(exp, .75, axis=0)
+        # q_25 = np.quantile(exp, .25, axis=0)
+        # mean = np.mean(exp, axis=0)
+        # plt.plot(mean, label=label)
+        # plt.fill_between(range(len(q_25)), q_25, q_75, alpha=0.1)
+
+    fig, ax = plt.subplots()
+
+    for time_usg, exp, ename in exp_list:
+        plot_exp(time_usg, exp, ename)
+    plt.grid()
+    plt.xlabel('Time in mins')
+    plt.ylabel('Test Accuracy')
+    plt.ylim(0.9700, 0.9815)
+
+    plt.xscale("log")
+    plt.xlim([0.01, 150])
+
+    plt.legend()
+    plt.title(title)
+    fig.savefig(f"./internal/ml/model_selection/exp_result/warm_up_move_proposal.pdf",
+                bbox_inches='tight')
+
+
 if __name__ == "__main__":
 
+    filter_full_train = filter_refinment_fully_training()
+    # warmups, move = export_warm_up_move_proposal("express_flow")
 
-    total_run = 5
-
-    ae_warmup_time, ae_warmup = [], []
-
-    for run_id in range(total_run):
-        _ae_warmup_time, _ae_warmup = run_evolution_search(
-            max_trained_models=100,
-            pool_size=64,
-            tournament_size=10,
-            zero_cost_warmup=3000,
-            zero_cost_move=False,
-            tfmem="express_flow",
-            dataset_name="frappe",
-            query_epoch=19,
-            args=args)
-
-        ae_warmup_time.append(_ae_warmup_time)
-        ae_warmup.append(_ae_warmup)
-
-    ae_move_time, ae_move = [], []
-    for run_id in range(total_run):
-        _ae_move_time, _ae_move = run_evolution_search(
-            max_trained_models=100,
-            pool_size=64,
-            tournament_size=10,
-            zero_cost_warmup=0,
-            zero_cost_move=True,
-            tfmem="express_flow",
-            dataset_name="frappe",
-            query_epoch=19,
-            args=args)
-
-        ae_move_time.append(_ae_move_time)
-        ae_move.append(_ae_move)
-
-    plot_experiment([(ae_warmup_time, ae_warmup, 'AE + warmup (3000)'),
-                     (ae_move_time, ae_move, 'AE + move')],
-                    'Aging Evolution Search')
+    plot_experiment(
+        [
+            # (warmups[0], warmups[1], warmups[2]),
+            # (move[0], move[1], move[2]),
+            (filter_full_train[0], filter_full_train[1], filter_full_train[2])
+         ],
+        'EA Search')
 
     print("Done")
