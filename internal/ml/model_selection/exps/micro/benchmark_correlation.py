ada2d873d4fa0dc55c759c1871334d0e85804071
working <57171759+NLGithubWP@users.noreply.github.com>
2023-08-08 19:40:00 +0800

    Update result

--- internal/ml/model_selection/exps/micro/benchmark_correlation.py
+++ internal/ml/model_selection/exps/micro/benchmark_correlation.py
@@ -20,8 +20,8 @@ def calculate_correlation(dataset, search_space, epoch_train, srcc_top_k: List =
     # Find the intersection between trained_models and scored_models
     trained_scored_models = list(set(trained_models) & set(scored_models))
 
-    # Initialize storage for algorithm scores and training results
-    model_train_res_lst = []
+    # each alg have one corresponding ground truth list
+    model_train_res_lst = {"nas_wot_add_syn_flow": []}
     all_alg_score_dic = {"nas_wot_add_syn_flow": []}
 
     # Populate algorithm scores and training results
@@ -29,31 +29,36 @@ def calculate_correlation(dataset, search_space, epoch_train, srcc_top_k: List =
         score_value = score_query.query_all_tfmem_score(model_id)
         acc, _ = acc_query.get_ground_truth(arch_id=model_id, dataset=dataset, epoch_num=epoch_train)
 
+        # append score and ground truth
         for alg, value in score_value.items():
             # If the algorithm is not in the dict, initialize its list
+            if alg not in model_train_res_lst:
+                model_train_res_lst[alg] = []
             if alg not in all_alg_score_dic:
                 all_alg_score_dic[alg] = []
 
+            model_train_res_lst[alg].append(acc)
             all_alg_score_dic[alg].append(float(value))
 
         if "nas_wot" in score_value:
             all_alg_score_dic["nas_wot_add_syn_flow"].append(
                 float(score_value["nas_wot"]) + float(score_value["synflow"]))
+            model_train_res_lst["nas_wot_add_syn_flow"].append(acc)
         else:
             all_alg_score_dic["nas_wot_add_syn_flow"].append(0)
-
-        model_train_res_lst.append(acc)
+            model_train_res_lst["nas_wot_add_syn_flow"].append(acc)
 
     # Measure the correlation for each algorithm and print the result
     for topp in srcc_top_k:
         print("--------------------------------------------------")
         for alg in all_alg_score_dic.keys():
             scores = all_alg_score_dic[alg]
-            sorted_indices = np.argsort(scores)[- int(topp * len(scores)):]
-            sorted_ground_truth = [model_train_res_lst[i] for i in sorted_indices]
-            sorted_scores = [scores[i] for i in sorted_indices]
-            res = CorCoefficient.measure(sorted_scores, sorted_ground_truth)
-            print(f"Top {topp, len(sorted_indices)}, {alg}, {res[CommonVars.Spearman]}")
+            ground_truth = model_train_res_lst[alg]
+            ground_truth_sorted_indices = np.argsort(ground_truth)[- int(topp * len(ground_truth)):]
+            top_ground_truth = [ground_truth[i] for i in ground_truth_sorted_indices]
+            top_scores = [scores[i] for i in ground_truth_sorted_indices]
+            res = CorCoefficient.measure(top_scores, top_ground_truth)
+            print(f"Top {topp, len(top_ground_truth)}, {alg}, {res[CommonVars.Spearman]}")
 
     # Get global ranks, measure correlation and print result for JACFLOW
     # if dataset in [Config.Frappe, Config.UCIDataset, Config.Criteo]:
@@ -62,7 +67,8 @@ def calculate_correlation(dataset, search_space, epoch_train, srcc_top_k: List =
                              for model_id in trained_scored_models]
 
         sorted_indices = np.argsort(global_rank_score)
-        sorted_ground_truth = [model_train_res_lst[i] for i in sorted_indices]
+        # here the ground truth list should be same as nas_wot.., since they share the same model list
+        sorted_ground_truth = [model_train_res_lst['nas_wot'][i] for i in sorted_indices]
         sorted_scores = [global_rank_score[i] for i in sorted_indices]
 
         res = CorCoefficient.measure(sorted_scores, sorted_ground_truth)
@@ -105,7 +111,7 @@ if __name__ == "__main__":
     from src.common.constant import CommonVars, Config
 
     # Frappe configuration, here also measure SRCC of top 0.2% -> 0.8%
-    calculate_correlation(Config.Frappe, Config.MLPSP, 19, srcc_top_k=[0.002, 0.004, 0.006, 0.008, 1])
+    calculate_correlation(Config.Frappe, Config.MLPSP, 19, srcc_top_k=[0.005, 1])
 
     # UCI configuration
     calculate_correlation(Config.UCIDataset, Config.MLPSP, 0)
@@ -126,7 +132,6 @@ if __name__ == "__main__":
     calculate_correlation(Config.imgNet, Config.NB201, None)
 
     """
-    Here is the output:
     ================================================================
      frappe + mlp_sp
     ================================================================
@@ -135,65 +140,20 @@ if __name__ == "__main__":
     Loading ../exp_data/tab_data/expressflow_score_mlp_sp_frappe_batch_size_32_cpu.json...
     Loading ../exp_data/tab_data/weight_share_nas_frappe.json...
     --------------------------------------------------
-    Top (0.002, 320), nas_wot_add_syn_flow, 0.09176383558433188
-    Top (0.002, 320), grad_norm, -0.05221181301609281
-    Top (0.002, 320), grad_plain, 0.010659033743417297
-    Top (0.002, 320), nas_wot, 0.17182172240830892
-    Top (0.002, 320), ntk_cond_num, 0.047184176603287144
-    Top (0.002, 320), ntk_trace, -0.19255177854077166
-    Top (0.002, 320), ntk_trace_approx, -0.03251309912000771
-    Top (0.002, 320), fisher, -0.11514936834521941
-    Top (0.002, 320), grasp, 0.08584539745341016
-    Top (0.002, 320), snip, 0.11704523741755678
-    Top (0.002, 320), synflow, 0.09176383558433188
-    Top (0.002, 320), weight_norm, 0.07500883959917261
-    Top (0.002, 320), express_flow, 0.17456750065918614
-    Top (0.002, 4), weight_share, -0.7999999999999999
-    --------------------------------------------------
-    Top (0.004, 640), nas_wot_add_syn_flow, 0.11882548541378278
-    Top (0.004, 640), grad_norm, 0.025383640373591164
-    Top (0.004, 640), grad_plain, -0.005632709232963978
-    Top (0.004, 640), nas_wot, 0.1366327900980159
-    Top (0.004, 640), ntk_cond_num, -0.15135665635044113
-    Top (0.004, 640), ntk_trace, -0.1648806092409808
-    Top (0.004, 640), ntk_trace_approx, -0.11244283819712042
-    Top (0.004, 640), fisher, -0.13527564278345275
-    Top (0.004, 640), grasp, 0.08921729237998346
-    Top (0.004, 640), snip, 0.10973746428064046
-    Top (0.004, 640), synflow, 0.11882763690829326
-    Top (0.004, 640), weight_norm, 0.1397656899871464
-    Top (0.004, 640), express_flow, 0.1770325672181817
-    Top (0.004, 8), weight_share, -0.523809523809524
-    --------------------------------------------------
-    Top (0.006, 960), nas_wot_add_syn_flow, 0.14857992738707398
-    Top (0.006, 960), grad_norm, 0.06158907308879807
-    Top (0.006, 960), grad_plain, -0.019561768676986176
-    Top (0.006, 960), nas_wot, 0.11271946601821807
-    Top (0.006, 960), ntk_cond_num, -0.14027904408911906
-    Top (0.006, 960), ntk_trace, -0.17359733075154035
-    Top (0.006, 960), ntk_trace_approx, -0.13465470597426205
-    Top (0.006, 960), fisher, -0.10129184676317263
-    Top (0.006, 960), grasp, 0.0773428333918988
-    Top (0.006, 960), snip, 0.08243519772399532
-    Top (0.006, 960), synflow, 0.14858087682386803
-    Top (0.006, 960), weight_norm, 0.15950466273961506
-    Top (0.006, 960), express_flow, 0.18887676473173254
-    Top (0.006, 12), weight_share, -0.6083916083916086
-    --------------------------------------------------
-    Top (0.008, 1280), nas_wot_add_syn_flow, 0.17996247022245493
-    Top (0.008, 1280), grad_norm, 0.06243788518998898
-    Top (0.008, 1280), grad_plain, -0.011124894709764077
-    Top (0.008, 1280), nas_wot, 0.12083829746089553
-    Top (0.008, 1280), ntk_cond_num, -0.1371495342350461
-    Top (0.008, 1280), ntk_trace, -0.18678413362379745
-    Top (0.008, 1280), ntk_trace_approx, -0.11327261499399259
-    Top (0.008, 1280), fisher, -0.1335225388835811
-    Top (0.008, 1280), grasp, 0.08311089873786662
-    Top (0.008, 1280), snip, 0.11391344623983912
-    Top (0.008, 1280), synflow, 0.1799591113794625
-    Top (0.008, 1280), weight_norm, 0.17665909042477856
-    Top (0.008, 1280), express_flow, 0.19513000381775134
-    Top (0.008, 16), weight_share, -0.18529411764705883
+    Top (0.005, 800), nas_wot_add_syn_flow, 0.1986753573052458
+    Top (0.005, 800), grad_norm, 0.12076418883266768
+    Top (0.005, 800), grad_plain, 0.006358620227609798
+    Top (0.005, 800), nas_wot, 0.19057972309084753
+    Top (0.005, 800), ntk_cond_num, -0.0466050197351888
+    Top (0.005, 800), ntk_trace, 0.08758335876257788
+    Top (0.005, 800), ntk_trace_approx, 0.05574130182201712
+    Top (0.005, 800), fisher, 0.0641524012888946
+    Top (0.005, 800), grasp, -0.018840689722475004
+    Top (0.005, 800), snip, 0.19218164902774643
+    Top (0.005, 800), synflow, 0.1986753573052458
+    Top (0.005, 800), weight_norm, 0.20104446565490514
+    Top (0.005, 800), express_flow, 0.22902225628477543
+    Top (0.005, 10), weight_share, 0.43030303030303024
     --------------------------------------------------
     Top (1, 160000), nas_wot_add_syn_flow, 0.7722624738302497
     Top (1, 160000), grad_norm, 0.5022881504556264
@@ -208,7 +168,7 @@ if __name__ == "__main__":
     Top (1, 160000), synflow, 0.7721962598662835
     Top (1, 160000), weight_norm, 0.7473390604383027
     Top (1, 160000), express_flow, 0.8028456677612497
-    Top (1, 2000), weight_share, -0.03136309834077458
+    Top (1, 2000), weight_share, 0.517565395891349
     JACFLOW 0.7406547545980877
     
     ================================================================
@@ -263,6 +223,7 @@ if __name__ == "__main__":
      cifar10 + nasbench101
     ================================================================
     Loading ../exp_data/img_data/score_101_15k_c10_128.json...
+    Loading ../exp_data/img_data/expssflow_score_nasbench101_cifar10_batch_size_32_cpu.json...
     Loading pickel ../exp_data/img_data/ground_truth/nasbench1_accuracy.p...
     Loading ../exp_data/img_data/ground_truth/nb101_id_to_hash.json...
     Loading ../exp_data/img_data/ground_truth/101_allEpoch_info_json...
@@ -280,12 +241,14 @@ if __name__ == "__main__":
     Top (1, 15625), snip, -0.1668017404578911
     Top (1, 15625), synflow, 0.3689520399131206
     Top (1, 15625), weight_norm, 0.5332072603621669
+    Top (1, 73), express_flow, 0.3754348914881334
     JACFLOW 0.412358290561836
     
     ================================================================
      cifar10 + nasbench201
     ================================================================
     Loading ../exp_data/img_data/score_201_15k_c10_bs32_ic16.json...
+    Loading ../exp_data/img_data/expssflow_score_nasbench201_cifar10_batch_size_32_cpu.json...
     Loading ../exp_data/img_data/ground_truth/201_allEpoch_info...
     --------------------------------------------------
     Top (1, 15625), nas_wot_add_syn_flow, 0.778881359475867
@@ -300,12 +263,14 @@ if __name__ == "__main__":
     Top (1, 15625), snip, 0.6437364743868734
     Top (1, 15625), weight_norm, 0.005565168122791671
     Top (1, 15625), synflow, 0.7788685936507451
+    Top (1, 2001), express_flow, 0.7808596435404074
     JACFLOW 0.8339798847659702
     
     ================================================================
      cifar100 + nasbench201
     ================================================================
     Loading ../exp_data/img_data/score_201_15k_c100_bs32_ic16.json...
+    Loading ../exp_data/img_data/expssflow_score_nasbench201_cifar100_batch_size_32_cpu.json...
     --------------------------------------------------
     Top (1, 15000), nas_wot_add_syn_flow, 0.7671956422900841
     Top (1, 15000), grad_norm, 0.638398024328767
@@ -319,12 +284,14 @@ if __name__ == "__main__":
     Top (1, 15000), snip, 0.6375851983100865
     Top (1, 15000), weight_norm, 0.011918450096024165
     Top (1, 15000), synflow, 0.7671950896881894
+    Top (1, 1920), express_flow, 0.7662904274045729
     JACFLOW 0.836747529703176
     
     ================================================================
      ImageNet16-120 + nasbench201
     ================================================================
     Loading ../exp_data/img_data/score_201_15k_imgNet_bs32_ic16.json...
+    Loading ../exp_data/img_data/expssflow_score_nasbench201_ImageNet16-120_batch_size_32_cpu.json...
     --------------------------------------------------
     Top (1, 15000), nas_wot_add_syn_flow, 0.7450159967363084
     Top (1, 15000), grad_norm, 0.566172650250217
@@ -338,6 +305,6 @@ if __name__ == "__main__":
     Top (1, 15000), snip, 0.5688946225005688
     Top (1, 15000), weight_norm, 0.005571648346911519
     Top (1, 15000), synflow, 0.7450170886565295
+    Top (1, 1920), express_flow, 0.743078455158988
     JACFLOW 0.8077842182522329
-
-    """
+"""
