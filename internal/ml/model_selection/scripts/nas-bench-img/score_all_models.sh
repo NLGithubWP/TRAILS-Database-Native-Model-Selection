ca9bd65a49c302d0cc5d51343d3fb9c538da87b4
working <57171759+NLGithubWP@users.noreply.github.com>
2023-08-09 00:23:15 +0800

    Repeat running

--- internal/ml/model_selection/scripts/nas-bench-img/score_all_models.sh
+++ internal/ml/model_selection/scripts/nas-bench-img/score_all_models.sh
@@ -4,55 +4,57 @@
 export PYTHONPATH=$PYTHONPATH:./internal/ml/model_selection
 
 
-# cifar10 + nb101
-python ./internal/ml/model_selection/exps/nas_bench_tabular/4.seq_score_online.py \
-  --models_explore=15000 \
-  --search_space=nasbench101 \
-  --api_loc=nasbench_only108.pkl \
-  --base_dir=/hdd1/xingnaili/exp_data/ \
-  --dataset=cifar10 \
-  --batch_size=32 \
-  --num_labels=10 \
-  --device=cpu \
-  --log_folder=log_score_all_img10_101 \
-  --result_dir=./internal/ml/model_selection/exp_result/
+for i in {1..4}
+do
+    # cifar10 + nb101
+    python ./internal/ml/model_selection/exps/nas_bench_tabular/4.seq_score_online.py \
+      --models_explore=3000 \
+      --search_space=nasbench101 \
+      --api_loc=nasbench_only108.pkl \
+      --base_dir=/hdd1/xingnaili/exp_data/ \
+      --dataset=cifar10 \
+      --batch_size=32 \
+      --num_labels=10 \
+      --device=cpu \
+      --log_folder=log_score_all_img10_101 \
+      --result_dir=./internal/ml/model_selection/exp_result/
 
+    # cifar10 + nb201
+    python ./internal/ml/model_selection/exps/nas_bench_tabular/4.seq_score_online.py \
+      --models_explore=3000 \
+      --search_space=nasbench201 \
+      --api_loc=NAS-Bench-201-v1_1-096897.pth \
+      --base_dir=/hdd1/xingnaili/exp_data/ \
+      --dataset=cifar10 \
+      --batch_size=32 \
+      --num_labels=10 \
+      --device=cpu \
+      --log_folder=log_score_all_img10 \
+      --result_dir=./internal/ml/model_selection/exp_result/
 
-# cifar10 + nb201
-python ./internal/ml/model_selection/exps/nas_bench_tabular/4.seq_score_online.py \
-  --models_explore=15000 \
-  --search_space=nasbench201 \
-  --api_loc=NAS-Bench-201-v1_1-096897.pth \
-  --base_dir=/hdd1/xingnaili/exp_data/ \
-  --dataset=cifar10 \
-  --batch_size=32 \
-  --num_labels=10 \
-  --device=cpu \
-  --log_folder=log_score_all_img10 \
-  --result_dir=./internal/ml/model_selection/exp_result/
+    # cifar100 + nb201
+    python ./internal/ml/model_selection/exps/nas_bench_tabular/4.seq_score_online.py \
+      --models_explore=3000 \
+      --search_space=nasbench201 \
+      --api_loc=NAS-Bench-201-v1_1-096897.pth \
+      --base_dir=/hdd1/xingnaili/exp_data/ \
+      --dataset=cifar100 \
+      --batch_size=32 \
+      --num_labels=100 \
+      --device=cpu \
+      --log_folder=log_score_all_img100 \
+      --result_dir=./internal/ml/model_selection/exp_result/
 
-# cifar100 + nb201
-python ./internal/ml/model_selection/exps/nas_bench_tabular/4.seq_score_online.py \
-  --models_explore=15000 \
-  --search_space=nasbench201 \
-  --api_loc=NAS-Bench-201-v1_1-096897.pth \
-  --base_dir=/hdd1/xingnaili/exp_data/ \
-  --dataset=cifar100 \
-  --batch_size=32 \
-  --num_labels=100 \
-  --device=cpu \
-  --log_folder=log_score_all_img100 \
-  --result_dir=./internal/ml/model_selection/exp_result/
-
-# imgnet + nb201
-python ./internal/ml/model_selection/exps/nas_bench_tabular/4.seq_score_online.py \
-  --models_explore=15000 \
-  --search_space=nasbench201 \
-  --api_loc=NAS-Bench-201-v1_1-096897.pth \
-  --base_dir=/hdd1/xingnaili/exp_data/ \
-  --dataset=ImageNet16-120 \
-  --batch_size=32 \
-  --num_labels=120 \
-  --device=cpu \
-  --log_folder=log_score_all_img_imgnet \
-  --result_dir=./internal/ml/model_selection/exp_result/
+    # imgnet + nb201
+    python ./internal/ml/model_selection/exps/nas_bench_tabular/4.seq_score_online.py \
+      --models_explore=3000 \
+      --search_space=nasbench201 \
+      --api_loc=NAS-Bench-201-v1_1-096897.pth \
+      --base_dir=/hdd1/xingnaili/exp_data/ \
+      --dataset=ImageNet16-120 \
+      --batch_size=32 \
+      --num_labels=120 \
+      --device=cpu \
+      --log_folder=log_score_all_img_imgnet \
+      --result_dir=./internal/ml/model_selection/exp_result/
+done
