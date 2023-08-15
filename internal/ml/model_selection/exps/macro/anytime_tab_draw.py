a820548172e34cab51b8ca67e9c341ff9e8288db
working <57171759+NLGithubWP@users.noreply.github.com>
2023-08-15 22:50:02 +0800

    Macro frappe test

--- internal/ml/model_selection/exps/macro/anytime_tab_draw.py
+++ internal/ml/model_selection/exps/macro/anytime_tab_draw.py
@@ -5,43 +5,48 @@ from src.tools.io_tools import read_json
 from exps.draw_tab_lib import draw_structure_data_anytime
 
 trian_time = 3
+
+
 def get_dataset_parameters(dataset):
     parameters = {
         "uci_diabetes": {
             "epoch": 0,
-            "sys_end2end_res": "./internal/ml/model_selection/exp_result/res_end_2_end_uci_diabetes_100_5.json",
-            "sys_end2end_p1": "./internal/ml/model_selection/exp_result/res_end_2_end_uci_diabetes_100_5_p1.json",
-            "tab_nas_res": "./tabNAS_benchmark_uci_diabetes_epoch_0.json",
+            "sys_end2end_res": "./internal/ml/model_selection/exp_result/res_end_2_end_mlp_sp_frappe_-1_10_express_flow.json",
+            "sys_end2end_p1": "./internal/ml/model_selection/exp_result/res_end_2_end_mlp_sp_frappe_-1_10_express_flow_p1.json",
+            "tab_nas_res": "./internal/ml/model_selection/exp_result/tabNAS_benchmark_uci_diabetes_epoch_0.json",
+            "train_based_re": "",
             "mx_value": 67.4,
             "y_lim": [61.8, 67.5],
             "figure_size": (6.2, 4.71),
             "datasetfg_name": "Diabetes",
             "annotations": [],  # ["TabNAS", 63.33, 8.14/60],
-            "remove_n_points": 1,
+            "remove_n_points": 0,
         },
         "frappe": {
             "epoch": 19,
-            "sys_end2end_res": "./internal/ml/model_selection/exp_result/res_end_2_end_frappe_100_5.json",
-            "sys_end2end_p1": "./internal/ml/model_selection/exp_result/res_end_2_end_frappe_100_5_p1.json",
-            "tab_nas_res": "./tabNAS_benchmark_frappe_epoch_19.json",
+            "sys_end2end_res": "./internal/ml/model_selection/exp_result/res_end_2_end_mlp_sp_frappe_-1_10_express_flow.json",
+            "sys_end2end_p1": "./internal/ml/model_selection/exp_result/res_end_2_end_mlp_sp_frappe_-1_10_express_flow_p1.json",
+            "tab_nas_res": "./internal/ml/model_selection/exp_result/tabNAS_benchmark_frappe_epoch_19.json",
+            "train_based_re": "./internal/ml/model_selection/exp_result/train_base_line_re_frappe_epoch_19.json",
             "mx_value": 98.052,
             "y_lim": [97.6, None],
             "figure_size": (6.2, 4.71),
             "datasetfg_name": dataset,
             "annotations": [],  # ["TabNAS", 97.68, 324.8/60],
-            "remove_n_points": 2,
+            "remove_n_points": 0,
         },
         "criteo": {
             "epoch": 9,
             "sys_end2end_res": "./internal/ml/model_selection/exp_result/res_end_2_end_criteo_100_5.json",
             "sys_end2end_p1": "./internal/ml/model_selection/exp_result/res_end_2_end_criteo_100_5_p1.json",
-            "tab_nas_res": "./tabNAS_benchmark_criteo_epoch_9.json",
+            "tab_nas_res": "./internal/ml/model_selection/exp_result/tabNAS_benchmark_criteo_epoch_9.json",
+            "train_based_re": "",
             "mx_value": 80.335,
             "y_lim": [80.1, None],
             "figure_size": (6.2, 4.71),
             "datasetfg_name": dataset,
             "annotations": [],  # ["TabNAS", 80.17, 7250.0/60],
-            "remove_n_points": 3,
+            "remove_n_points": 0,
         },
     }
 
@@ -70,16 +75,16 @@ def generate_and_draw_data(dataset):
 
     result_dir = "./internal/ml/model_selection/exp_result/"
 
-    train_based_res = read_json(
-        f"{result_dir}/res_train_base_line_{dataset}_epoch_{params['epoch']}.json")
-    sampled_train_x, sampled_train_y = sample_some_points(x_array=train_based_res["baseline_time_budget"],
-                                                          y_2d_array=train_based_res["baseline_acc"],
-                                                          save_points=9,
-                                                          remove_n_points=params['remove_n_points'])
-
     system_result = read_json(params['sys_end2end_res'])
     system_p1_result = read_json(params['sys_end2end_p1'])
     tab_nas_res = read_json(params["tab_nas_res"])
+    train_based_res = read_json(params["train_based_re"])
+
+    sampled_train_x, sampled_train_y = sample_some_points(
+        x_array=[[earch * trian_time for earch in ele] for ele in train_based_res["sys_time_budget"]],
+        y_2d_array=train_based_res["sys_acc"],
+        save_points=9,
+        remove_n_points=0)
 
     sampled_sys_x, sampled_sys_y = sample_some_points(
         x_array=[system_result["sys_time_budget"] for _ in system_result["sys_acc"]],
@@ -88,10 +93,10 @@ def generate_and_draw_data(dataset):
         remove_n_points=0)
 
     tabnas_x, tabnas_y = sample_some_points(
-        x_array=[[earch * trian_time for earch in ele] for ele in tab_nas_res["baseline_time_budget"]],
-        y_2d_array=tab_nas_res["baseline_acc"],
+        x_array=[[earch * trian_time for earch in ele] for ele in tab_nas_res["sys_time_budget"]],
+        y_2d_array=tab_nas_res["sys_acc"],
         save_points=9,
-        remove_n_points=3)
+        remove_n_points=0)
 
     all_lines = [
         [sampled_train_x, sampled_train_y, "Training-Based MS"],
@@ -111,9 +116,9 @@ def generate_and_draw_data(dataset):
         x_ticks=[0.01, None]
     )
 
+
 # Choose dataset to process
 dataset = "frappe"
 # dataset = "uci_diabetes"
 # dataset = "criteo"
 generate_and_draw_data(dataset)
-
