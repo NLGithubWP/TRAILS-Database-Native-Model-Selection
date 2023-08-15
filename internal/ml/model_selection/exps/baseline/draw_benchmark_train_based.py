0de4aff0d0a2de00c4999cf0ac839baec43b4542
working <57171759+NLGithubWP@users.noreply.github.com>
2023-08-15 21:41:16 +0800

    Update figure

--- internal/ml/model_selection/exps/baseline/draw_benchmark_train_based.py
+++ internal/ml/model_selection/exps/baseline/draw_benchmark_train_based.py
@@ -28,14 +28,15 @@ def sample_some_points(x_array, y_2d_array, save_points, remove_n_points=1) -> (
 
 
 # 'frappe, criteo, uci_diabetes'
-dataset = "frappe"
-# dataset = "criteo"
+# dataset = "frappe"
+dataset = "criteo"
 # dataset = "uci_diabetes"
 
 if dataset == "uci_diabetes":
     epoch = 0
     mx_value = 67.4
-    y_lim = [None, None]
+    y_lim = [60, None]
+    x_lim = [1.2, 1600]
     figure_size = (6.4, 3.8)
     datasetfg_name = "Diabetes"
     remove_n_points = 1
@@ -43,7 +44,8 @@ if dataset == "uci_diabetes":
 elif dataset == "frappe":
     epoch = 19
     mx_value = 98.08
-    y_lim = [None, None]
+    y_lim = [97.6, None]
+    x_lim = [2.1, 1600]
     figure_size = (6.4, 4)
     datasetfg_name = dataset
     remove_n_points = 1
@@ -52,7 +54,8 @@ elif dataset == "frappe":
 elif dataset == "criteo":
     epoch = 9
     mx_value = 80.335
-    y_lim = [None, None]
+    y_lim = [80.24, 80.35]
+    x_lim = [2.1, 1600]
     figure_size = (6.4, 4)
     datasetfg_name = dataset
     annotations = []
@@ -61,38 +64,34 @@ else:
     pass
 
 train_based_res_ea = read_json(f"./internal/ml/model_selection/exp_result/train_base_line_re_{dataset}_epoch_{epoch}.json")
-print(f"reading from {train_based_res_ea}")
 sampled_ea_x, sampled_ea_y = sample_some_points(x_array=train_based_res_ea["sys_time_budget"],
                                                 y_2d_array=train_based_res_ea["sys_acc"],
                                                 save_points=9,
                                                 remove_n_points=remove_n_points)
 
 train_based_res_rl = read_json(f"./internal/ml/model_selection/exp_result/train_base_line_rl_{dataset}_epoch_{epoch}.json")
-print(f"reading from {train_based_res_rl}")
 sampled_el_x, sampled_el_y = sample_some_points(x_array=train_based_res_rl["sys_time_budget"],
                                                 y_2d_array=train_based_res_rl["sys_acc"],
                                                 save_points=9,
                                                 remove_n_points=remove_n_points)
 
 train_based_res_rs = read_json(f"./internal/ml/model_selection/exp_result/train_base_line_rs_{dataset}_epoch_{epoch}.json")
-print(f"reading from {train_based_res_rs}")
 sampled_rs_x, sampled_rs_y = sample_some_points(x_array=train_based_res_rs["sys_time_budget"],
                                                 y_2d_array=train_based_res_rs["sys_acc"],
                                                 save_points=9,
                                                 remove_n_points=remove_n_points)
 
-# train_based_res_rs = read_json(f"./internal/ml/model_selection/exp_result/train_base_line_bohb_{dataset}_epoch_{epoch}.json")
-# print(f"reading from {train_based_res_rs}")
-# sampled_bohb_x, sampled_bohb_y = sample_some_points(x_array=train_based_res_rs["sys_time_budget"],
-#                                                     y_2d_array=train_based_res_rs["sys_acc"],
-#                                                     save_points=9,
-#                                                     remove_n_points=remove_n_points)
+train_based_res_rs = read_json(f"./internal/ml/model_selection/exp_result/train_base_line_bohb_{dataset}_epoch_{epoch}.json")
+sampled_bohb_x, sampled_bohb_y = sample_some_points(x_array=train_based_res_rs["sys_time_budget"],
+                                                    y_2d_array=train_based_res_rs["sys_acc"],
+                                                    save_points=9,
+                                                    remove_n_points=remove_n_points)
 
 all_lines = [
     [sampled_ea_x, sampled_ea_y, "RE"],
     [sampled_el_x, sampled_el_y, "RL"],
     [sampled_rs_x, sampled_rs_y, "RS"],
-    # [sampled_bohb_x, sampled_bohb_y, "BOHB"]
+    [sampled_bohb_x, sampled_bohb_y, "BOHB"]
 ]
 
 draw_structure_data_anytime(
@@ -103,5 +102,6 @@ draw_structure_data_anytime(
     figure_size=figure_size,
     annotations=annotations,
     y_ticks=y_lim,
-    x_ticks=[1, None]
+    x_ticks=x_lim,
+    unique_labels=["Global Best AUC","BOHB", "RL", "RS", "RE" ]
 )
