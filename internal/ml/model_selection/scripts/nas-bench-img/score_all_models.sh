cc24c0c067ead6519946a66f4000446d86be95e9
working <57171759+NLGithubWP@users.noreply.github.com>
2023-08-15 19:40:52 +0800

    Remove 101 temp

--- internal/ml/model_selection/scripts/nas-bench-img/score_all_models.sh
+++ internal/ml/model_selection/scripts/nas-bench-img/score_all_models.sh
@@ -7,17 +7,17 @@ export PYTHONPATH=$PYTHONPATH:./internal/ml/model_selection
 for i in {1..4}
 do
     # cifar10 + nb101
-    /home/xingnaili/miniconda3/envs/trails/bin/python ./internal/ml/model_selection/exps/nas_bench_tabular/4.seq_score_online.py \
-      --models_explore=3000 \
-      --search_space=nasbench101 \
-      --api_loc=nasbench_only108.pkl \
-      --base_dir=/hdd1/xingnaili/exp_data/ \
-      --dataset=cifar10 \
-      --batch_size=32 \
-      --num_labels=10 \
-      --device=cpu \
-      --log_folder=log_score_all_img10_101 \
-      --result_dir=./internal/ml/model_selection/exp_result/
+#    /home/xingnaili/miniconda3/envs/trails/bin/python ./internal/ml/model_selection/exps/nas_bench_tabular/4.seq_score_online.py \
+#      --models_explore=3000 \
+#      --search_space=nasbench101 \
+#      --api_loc=nasbench_only108.pkl \
+#      --base_dir=/hdd1/xingnaili/exp_data/ \
+#      --dataset=cifar10 \
+#      --batch_size=32 \
+#      --num_labels=10 \
+#      --device=cuda:0 \
+#      --log_folder=log_score_all_img10_101 \
+#      --result_dir=./internal/ml/model_selection/exp_result/
 
     # cifar10 + nb201
     /home/xingnaili/miniconda3/envs/trails/bin/python ./internal/ml/model_selection/exps/nas_bench_tabular/4.seq_score_online.py \
