a3f12f2c20a4c907c8f3030dd0ad2678c3998d61
working <57171759+NLGithubWP@users.noreply.github.com>
2023-08-23 16:53:13 +0800

    Clean the caching

--- internal/ml/model_selection/scripts/latency_embedding_cache.sh
+++ internal/ml/model_selection/scripts/latency_embedding_cache.sh
@@ -1,15 +1,12 @@
 
 
 export PYTHONPATH=$PYTHONPATH:./internal/ml/model_selection
-conda activate trails
-
 
 # frappe
 python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py \
   --embedding_cache_filtering=True \
   --tfmem=express_flow \
   --models_explore=5000 \
-  --log_name=score_based \
   --search_space=mlp_sp \
   --num_layers=4 \
   --hidden_choice_len=20 \
@@ -22,15 +19,13 @@ python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py 
   --nfield=10 \
   --nemb=10 \
   --workers=0 \
-  --result_dir=./internal/ml/model_selection/exp_filter_cache/ \
-  --log_folder=log_score_time_frappe_cache
+  --result_dir=./internal/ml/model_selection/exp_filter_cache/
 
 #criteo
 python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py \
   --embedding_cache_filtering=True \
   --tfmem=express_flow \
   --models_explore=5000 \
-  --log_name=score_based \
   --search_space=mlp_sp \
   --num_layers=4 \
   --hidden_choice_len=10 \
@@ -43,15 +38,13 @@ python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py 
   --nfield=39 \
   --nemb=10 \
   --workers=0 \
-  --result_dir=./internal/ml/model_selection/exp_filter_cache/ \
-  --log_folder=log_score_time_frappe_cache
+  --result_dir=./internal/ml/model_selection/exp_filter_cache/
 
 # uci
 python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py \
   --embedding_cache_filtering=True \
   --tfmem=express_flow \
   --models_explore=5000 \
-  --log_name=score_based \
   --search_space=mlp_sp \
   --num_layers=4 \
   --hidden_choice_len=20 \
@@ -64,59 +57,7 @@ python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py 
   --nfield=43 \
   --nemb=10 \
   --workers=0 \
-  --result_dir=./internal/ml/model_selection/exp_filter_cache/ \
-  --log_folder=log_score_time_frappe_cache
-
-#
-## cifar 10
-#python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py \
-#  --embedding_cache_filtering=True \
-#  --tfmem=synflow \
-#  --models_explore=5000 \
-#  --log_name=score_based \
-#  --search_space=nasbench201 \
-#  --api_loc=NAS-Bench-201-v1_1-096897.pth \
-#  --base_dir=/hdd1/xingnaili/exp_data/ \
-#  --num_labels=10 \
-#  --device=cuda:1 \
-#  --batch_size=32 \
-#  --dataset=cifar10 \
-#  --result_dir=./internal/ml/model_selection/exp_filter_cache/ \
-#  --log_folder=log_score_time_frappe_cache
-#
-#
-## cifar 100
-#python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py \
-#  --embedding_cache_filtering=True \
-#  --tfmem=synflow \
-#  --models_explore=5000 \
-#  --log_name=score_based \
-#  --search_space=nasbench201 \
-#  --api_loc=NAS-Bench-201-v1_1-096897.pth \
-#  --base_dir=/hdd1/xingnaili/exp_data/ \
-#  --num_labels=100 \
-#  --device=cuda:1 \
-#  --batch_size=32 \
-#  --dataset=cifar100 \
-#  --result_dir=./internal/ml/model_selection/exp_filter_cache/ \
-#  --log_folder=log_score_time_frappe_cache
-#
-#
-## imageNet
-#python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py \
-#  --embedding_cache_filtering=True \
-#  --tfmem=synflow \
-#  --models_explore=5000 \
-#  --log_name=score_based \
-#  --search_space=nasbench201 \
-#  --api_loc=NAS-Bench-201-v1_1-096897.pth \
-#  --base_dir=/hdd1/xingnaili/exp_data/ \
-#  --num_labels=120 \
-#  --device=cuda:1 \
-#  --batch_size=32 \
-#  --dataset=ImageNet16-120 \
-#  --result_dir=./internal/ml/model_selection/exp_filter_cache/ \
-#  --log_folder=log_score_time_frappe_cache
+  --result_dir=./internal/ml/model_selection/exp_filter_cache/
 
 ########################## CPU ##############################
 # this is run on cpu, only change the device==cpu for all above
@@ -126,7 +67,6 @@ python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py 
   --embedding_cache_filtering=True \
   --tfmem=express_flow \
   --models_explore=5000 \
-  --log_name=score_based \
   --search_space=mlp_sp \
   --num_layers=4 \
   --hidden_choice_len=20 \
@@ -139,15 +79,13 @@ python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py 
   --nfield=10 \
   --nemb=10 \
   --workers=0 \
-  --result_dir=./internal/ml/model_selection/exp_filter_cache/ \
-  --log_folder=log_score_time_frappe_cache
+  --result_dir=./internal/ml/model_selection/exp_filter_cache/
 
 #criteo
 python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py \
   --embedding_cache_filtering=True \
   --tfmem=express_flow \
   --models_explore=5000 \
-  --log_name=score_based \
   --search_space=mlp_sp \
   --num_layers=4 \
   --hidden_choice_len=10 \
@@ -160,15 +98,13 @@ python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py 
   --nfield=39 \
   --nemb=10 \
   --workers=0 \
-  --result_dir=./internal/ml/model_selection/exp_filter_cache/ \
-  --log_folder=log_score_time_frappe_cache
+  --result_dir=./internal/ml/model_selection/exp_filter_cache/
 
 # uci
 python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py \
   --embedding_cache_filtering=True \
   --tfmem=express_flow \
   --models_explore=5000 \
-  --log_name=score_based \
   --search_space=mlp_sp \
   --num_layers=4 \
   --hidden_choice_len=20 \
@@ -181,56 +117,6 @@ python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py 
   --nfield=43 \
   --nemb=10 \
   --workers=0 \
-  --result_dir=./internal/ml/model_selection/exp_filter_cache/ \
-  --log_folder=log_score_time_frappe_cache
+  --result_dir=./internal/ml/model_selection/exp_filter_cache/
 
 
-## cifar 10
-#python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py \
-#  --embedding_cache_filtering=True \
-#  --tfmem=synflow \
-#  --models_explore=5000 \
-#  --log_name=score_based \
-#  --search_space=nasbench201 \
-#  --api_loc=NAS-Bench-201-v1_1-096897.pth \
-#  --base_dir=/hdd1/xingnaili/exp_data/ \
-#  --num_labels=10 \
-#  --device=cpu \
-#  --batch_size=32 \
-#  --dataset=cifar10 \
-#  --result_dir=./internal/ml/model_selection/exp_filter_cache/ \
-#  --log_folder=log_score_time_frappe_cache
-#
-#
-## cifar 100
-#python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py \
-#  --embedding_cache_filtering=True \
-#  --tfmem=synflow \
-#  --models_explore=5000 \
-#  --log_name=score_based \
-#  --search_space=nasbench201 \
-#  --api_loc=NAS-Bench-201-v1_1-096897.pth \
-#  --base_dir=/hdd1/xingnaili/exp_data/ \
-#  --num_labels=100 \
-#  --device=cpu \
-#  --batch_size=32 \
-#  --dataset=cifar100 \
-#  --result_dir=./internal/ml/model_selection/exp_filter_cache/ \
-#  --log_folder=log_score_time_frappe_cache
-#
-#
-## imageNet
-#python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py \
-#  --embedding_cache_filtering=True \
-#  --tfmem=synflow \
-#  --models_explore=5000 \
-#  --log_name=score_based \
-#  --search_space=nasbench201 \
-#  --api_loc=NAS-Bench-201-v1_1-096897.pth \
-#  --base_dir=/hdd1/xingnaili/exp_data/ \
-#  --num_labels=120 \
-#  --device=cpu \
-#  --batch_size=32 \
-#  --dataset=ImageNet16-120 \
-#  --result_dir=./internal/ml/model_selection/exp_filter_cache/ \
-#  --log_folder=log_score_time_frappe_cache
