1d2fe20c5a315c739c99837cb9bbb4393b583e14
working <57171759+NLGithubWP@users.noreply.github.com>
2023-08-23 21:04:25 +0800

    Default use cpu

--- internal/ml/model_selection/scripts/nas-bench-tabular/score_all_modesl_criteo.sh
+++ internal/ml/model_selection/scripts/nas-bench-tabular/score_all_modesl_criteo.sh
@@ -13,7 +13,7 @@ nohup  python ./internal/ml/model_selection/exps/nas_bench_tabular/4.seq_score_o
   --hidden_choice_len=10 \
   --base_dir=/hdd1/xingnaili/exp_data/ \
   --num_labels=2 \
-  --device=cuda:0 \
+  --device=cpu \
   --batch_size=32 \
   --dataset=criteo \
   --nfeat=2100000 \
