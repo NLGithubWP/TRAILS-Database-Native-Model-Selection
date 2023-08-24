b0d325374e5ac82582c844ee1d5b8512ccbc5b11
working <57171759+NLGithubWP@users.noreply.github.com>
2023-08-24 19:06:51 +0800

    Skip the already executed tasks

--- internal/ml/model_selection/scripts/latency_phase1_cpu_gpu.sh
+++ internal/ml/model_selection/scripts/latency_phase1_cpu_gpu.sh
@@ -20,7 +20,7 @@ python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py 
   --nfield=10 \
   --nemb=10 \
   --workers=0 \
-  --result_dir=./internal/ml/model_selection/exp_result/
+  --result_dir=./internal/ml/model_selection/exp_result_sever_wo_cache/
 
 #criteo
 python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py \
@@ -39,7 +39,7 @@ python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py 
   --nfield=39 \
   --nemb=10 \
   --workers=0 \
-  --result_dir=./internal/ml/model_selection/exp_result/
+  --result_dir=./internal/ml/model_selection/exp_result_sever_wo_cache/
 
 # uci
 python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py \
@@ -58,7 +58,7 @@ python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py 
   --nfield=43 \
   --nemb=10 \
   --workers=0 \
-  --result_dir=./internal/ml/model_selection/exp_result/
+  --result_dir=./internal/ml/model_selection/exp_result_sever_wo_cache/
 
 
 # cifar 10
@@ -73,7 +73,7 @@ python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py 
   --device=cuda:0 \
   --batch_size=32 \
   --dataset=cifar10 \
-  --result_dir=./internal/ml/model_selection/exp_result/
+  --result_dir=./internal/ml/model_selection/exp_result_sever_wo_cache/
 
 
 # cifar 100
@@ -88,7 +88,7 @@ python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py 
   --device=cuda:0 \
   --batch_size=32 \
   --dataset=cifar100 \
-  --result_dir=./internal/ml/model_selection/exp_result/
+  --result_dir=./internal/ml/model_selection/exp_result_sever_wo_cache/
 
 
 # imageNet
@@ -103,7 +103,7 @@ python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py 
   --device=cuda:0 \
   --batch_size=32 \
   --dataset=ImageNet16-120 \
-  --result_dir=./internal/ml/model_selection/exp_result/
+  --result_dir=./internal/ml/model_selection/exp_result_sever_wo_cache/
 
 ########################## CPU ##############################
 # this is run on cpu, only change the device==cpu for all above
@@ -125,7 +125,7 @@ python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py 
   --nfield=10 \
   --nemb=10 \
   --workers=0 \
-  --result_dir=./internal/ml/model_selection/exp_result/
+  --result_dir=./internal/ml/model_selection/exp_result_sever_wo_cache/
 
 #criteo
 python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py \
@@ -144,7 +144,7 @@ python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py 
   --nfield=39 \
   --nemb=10 \
   --workers=0 \
-  --result_dir=./internal/ml/model_selection/exp_result/
+  --result_dir=./internal/ml/model_selection/exp_result_sever_wo_cache/
 
 # uci
 python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py \
@@ -163,22 +163,22 @@ python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py 
   --nfield=43 \
   --nemb=10 \
   --workers=0 \
-  --result_dir=./internal/ml/model_selection/exp_result/
+  --result_dir=./internal/ml/model_selection/exp_result_sever_wo_cache/
 
 
 # cifar 10
-python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py \
-  --embedding_cache_filtering=False \
-  --tfmem=express_flow \
-  --models_explore=5000 \
-  --search_space=nasbench201 \
-  --api_loc=NAS-Bench-201-v1_1-096897.pth \
-  --base_dir=/hdd1/xingnaili/exp_data/ \
-  --num_labels=10 \
-  --device=cpu \
-  --batch_size=32 \
-  --dataset=cifar10 \
-  --result_dir=./internal/ml/model_selection/exp_result/
+#python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py \
+#  --embedding_cache_filtering=False \
+#  --tfmem=express_flow \
+#  --models_explore=5000 \
+#  --search_space=nasbench201 \
+#  --api_loc=NAS-Bench-201-v1_1-096897.pth \
+#  --base_dir=/hdd1/xingnaili/exp_data/ \
+#  --num_labels=10 \
+#  --device=cpu \
+#  --batch_size=32 \
+#  --dataset=cifar10 \
+#  --result_dir=./internal/ml/model_selection/exp_result_sever_wo_cache/
 
 
 # cifar 100
@@ -193,7 +193,7 @@ python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py 
   --device=cpu \
   --batch_size=32 \
   --dataset=cifar100 \
-  --result_dir=./internal/ml/model_selection/exp_result/
+  --result_dir=./internal/ml/model_selection/exp_result_sever_wo_cache/
 
 
 # imageNet
@@ -208,4 +208,4 @@ python3 ./internal/ml/model_selection/exps/micro/benchmark_filtering_latency.py 
   --device=cpu \
   --batch_size=32 \
   --dataset=ImageNet16-120 \
-  --result_dir=./internal/ml/model_selection/exp_result/
+  --result_dir=./internal/ml/model_selection/exp_result_sever_wo_cache/
