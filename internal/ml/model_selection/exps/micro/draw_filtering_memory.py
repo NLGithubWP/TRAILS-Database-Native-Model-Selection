c68b89a49633fd646d0a7c42fa6448adec1d4109
working <57171759+NLGithubWP@users.noreply.github.com>
2023-08-24 20:45:29 +0800

    Adjust the size

--- internal/ml/model_selection/exps/micro/draw_filtering_memory.py
+++ internal/ml/model_selection/exps/micro/draw_filtering_memory.py
@@ -70,7 +70,7 @@ def plot_memory_usage(params, interval=0.5):
 
     fig = plt.figure(figsize=(6.4, 4.5))
     # Adjust the space between
-    fig.subplots_adjust(hspace=0.3)
+    fig.subplots_adjust(hspace=0.25)
 
     # 1. first plot
     ax_gpu = fig.add_subplot(gs[0])
