8769135f5b747b11ed638a0f9f98c7a7bfe571fe
working <57171759+NLGithubWP@users.noreply.github.com>
2023-09-01 17:20:32 +0800

    Capture error msg

--- internal/pg_extension/src/bindings/model.rs
+++ internal/pg_extension/src/bindings/model.rs
@@ -4,16 +4,16 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub(crate) struct Frappe {
-    id: i32,
-    label: i32,
-    col1: String,
-    col2: String,
-    col3: String,
-    col4: String,
-    col5: String,
-    col6: String,
-    col7: String,
-    col8: String,
-    col9: String,
-    col10: String,
+    pub(crate) id: i32,
+    pub(crate) label: i32,
+    pub(crate) col1: String,
+    pub(crate) col2: String,
+    pub(crate) col3: String,
+    pub(crate) col4: String,
+    pub(crate) col5: String,
+    pub(crate) col6: String,
+    pub(crate) col7: String,
+    pub(crate) col8: String,
+    pub(crate) col9: String,
+    pub(crate) col10: String,
 }
