3e87177f7832f60d89f04a97ca3149d8699467cc
working <57171759+NLGithubWP@users.noreply.github.com>
2023-09-06 16:32:07 +0800

    Debug pg interface

--- internal/ml/model_selection/eva_service.py
+++ internal/ml/model_selection/eva_service.py
@@ -66,26 +66,6 @@ async def start_refinement_phase(request):
 
 
 if __name__ == "__main__":
-    import requests
-
-    url = 'http://localhost:8093/'
-    columns = ['label', 'col1', 'col2', 'col3', 'col4', 'col5', 'col6', 'col7', 'col8', 'col9', 'col10']
-    response = requests.post(
-        url, json={'columns': columns,
-                   'name_space': "train",
-                   'table_name': "frappe_train",
-                   "batch_size": 32})
-    print(response.json())
-
-    response = requests.post(
-        url, json={'columns': columns,
-                   'name_space': "valid",
-                   'table_name': "frappe_train",
-                   "batch_size": 32})
-    print(response.json())
-
-    # this is filtering phase
-    time.sleep(5)
 
     result = refinement_phase(
         u=1,
