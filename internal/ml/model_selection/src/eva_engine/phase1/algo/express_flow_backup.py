a560372f5bb0015140f16d136cc18714fa635fa3
working <57171759+NLGithubWP@users.noreply.github.com>
2023-09-20 11:56:09 +0800

    Backup express flow

--- internal/ml/model_selection/src/eva_engine/phase1/algo/express_flow_backup.py
+++ internal/ml/model_selection/src/eva_engine/phase1/algo/express_flow_backup.py
@@ -2,6 +2,7 @@ from src.eva_engine.phase1.algo.alg_base import Evaluator
 from src.eva_engine.phase1.utils.autograd_hacks import *
 from torch import nn
 from src.common.constant import Config
+from functools import partial
 
 
 class IntegratedHook:
@@ -24,7 +25,9 @@ class IntegratedHook:
         self.activation_map[id(module)] = output
 
         # Register backward hook for gradient computation
-        output.register_hook(lambda grad: self.backward_hook(grad, module))
+        # todo: this will messed up the reference, result in the memory leak.
+        # output.register_hook(lambda grad: self.backward_hook(grad, module))
+        output.register_hook(partial(self.backward_hook, module=module))
 
     def backward_hook(self, grad, module):
         dz = grad  # gradient
@@ -34,10 +37,16 @@ class IntegratedHook:
         self.Vs.append(V)
 
     def calculate_trajectory_length(self, epsilon):
-        assert len(self.originals) == len(self.perturbations)
-        trajectory_lengths = [(x_perturbed - x).norm() / epsilon for x, x_perturbed in
+        # assert len(self.originals) == len(self.perturbations)
+        trajectory_lengths = [abs(x_perturbed - x).norm() / epsilon for x, x_perturbed in
                               zip(self.originals, self.perturbations)]
         return trajectory_lengths
+
+    def clear_all(self):
+        self.originals.clear()
+        self.perturbations.clear()
+        self.Vs.clear()
+        self.activation_map.clear()
 
 
 class ExpressFlowEvaluator(Evaluator):
@@ -63,8 +72,12 @@ class ExpressFlowEvaluator(Evaluator):
                  space_name: str) -> float:
 
         # Step 1: Linearize
-        signs = self.linearize(arch)
-        arch.double()
+        if space_name == Config.MLPSP:
+            signs = self.linearize(arch.mlp)
+            arch.mlp.double()
+        else:
+            signs = self.linearize(arch)
+            arch.double()
 
         hook_obj = IntegratedHook()
         hooks = []
@@ -94,29 +107,23 @@ class ExpressFlowEvaluator(Evaluator):
         # directly sum
         torch.sum(out).backward()
 
+        # total_sum = self.compute_score(trajectory_lengths, hook_obj.Vs)
         total_sum = self.weighted_score(trajectory_lengths, hook_obj.Vs)
-        # total_sum = self.compute_score_early_halflayers(out, Vs)
+        # total_sum = self.weighted_score_traj(trajectory_lengths, hook_obj.Vs)
+        # total_sum = self.weighted_score_width(trajectory_lengths, hook_obj.Vs)
+
+        # Step 2: Nonlinearize
+        if space_name == Config.MLPSP:
+            self.nonlinearize(arch.mlp, signs)
+        else:
+            self.nonlinearize(arch, signs)
 
         # Remove the hooks
-        # Step 2: Nonlinearize
-        self.nonlinearize(arch, signs)
+        for hook in hooks:
+            hook.remove()
+        del hooks
+        hook_obj.clear_all()
 
-        return total_sum
-
-    def compute_score_early_halflayers(self, out, Vs):
-        # Vs is a list of tensors, where each tensor corresponds to the product
-        # V=z×∣dz∣ (where z is the activation and dz is the gradient) for every ReLU layer in model.
-        # Each tensor in Vs has the shape (batch_size, number_of_neurons)
-        # 1. aggregates the importance of all neurons in that specific ReLU module.
-        # 2. only use the first half layers.
-
-        # Determine the half point
-        half_point = len(Vs) // 2
-
-        # Sum over the second half of the modules,
-        # Vs[i].shape[1]: number of neuron in the layer i
-        total_sum = sum(V.flatten().sum() * V.shape[1] for V in Vs[half_point:]) / 10
-        total_sum = total_sum.item()
         return total_sum
 
     def weighted_score(self, trajectory_lengths, Vs):
@@ -130,7 +137,50 @@ class ExpressFlowEvaluator(Evaluator):
 
         # Use the normalized trajectory lengths as weights for your total_sum
         total_sum = sum(
-            normalized_length * V.flatten().sum() * V.shape[1] for normalized_length, V in zip(normalized_lengths, Vs))
-        total_sum = total_sum.item()
+            normalized_length * V.flatten().sum() * V.shape[1]
+            for normalized_length, V in zip(normalized_lengths, Vs))
+        total_sum = total_sum
 
+        return total_sum
+
+    def weighted_score_traj(self, trajectory_lengths, Vs):
+        trajectory_lengths.reverse()
+        # Modify trajectory_lengths to ensure that deeper layers have smaller weights
+        # For example, by taking the inverse of each computed trajectory length.
+        inverse_trajectory_lengths = [1.0 / (length + 1e-6) for length in trajectory_lengths]
+
+        # Normalize trajectory lengths if needed (this ensures the weights aren't too large)
+        normalized_lengths = [length / sum(inverse_trajectory_lengths) for length in inverse_trajectory_lengths]
+
+        # Use the normalized trajectory lengths as weights for your total_sum
+        total_sum = sum(
+            normalized_length * V.flatten().sum() for normalized_length, V in zip(normalized_lengths, Vs))
+        total_sum = total_sum
+
+        return total_sum
+
+    def weighted_score_width(self, trajectory_lengths, Vs):
+        # Vs is a list of tensors, where each tensor corresponds to the product
+        # V=z×∣dz∣ (where z is the activation and dz is the gradient) for every ReLU layer in model.
+        # Each tensor in Vs has the shape (batch_size, number_of_neurons)
+        # 1. aggregates the importance of all neurons in that specific ReLU module.
+        # 2. only use the first half layers.
+
+        # Sum over the second half of the modules,
+        # Vs[i].shape[1]: number of neuron in the layer i
+        total_sum = sum(V.flatten().sum() * V.shape[1] for V in Vs) / 10
+        total_sum = total_sum
+        return total_sum
+
+    def compute_score(self, trajectory_lengths, Vs):
+        # Vs is a list of tensors, where each tensor corresponds to the product
+        # V=z×∣dz∣ (where z is the activation and dz is the gradient) for every ReLU layer in model.
+        # Each tensor in Vs has the shape (batch_size, number_of_neurons)
+        # 1. aggregates the importance of all neurons in that specific ReLU module.
+        # 2. only use the first half layers.
+
+        # Sum over the second half of the modules,
+        # Vs[i].shape[1]: number of neuron in the layer i
+        total_sum = sum(V.flatten().sum() for V in Vs) / 10
+        total_sum = total_sum
         return total_sum
