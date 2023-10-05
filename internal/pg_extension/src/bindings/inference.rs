eb8bcb8bfdc83850ebd17e738bb4b693922d11fe
working <57171759+NLGithubWP@users.noreply.github.com>
2023-10-05 18:07:12 +0800

    asdf

--- internal/pg_extension/src/bindings/inference.rs
+++ internal/pg_extension/src/bindings/inference.rs
@@ -434,7 +434,7 @@ pub fn run_sams_inference_shared_memory_write_once(
     run_python_function(
         &PY_MODULE_SAMS,
         &eva_task_json,
-        "model_inference_compute_shared_memory");
+        "model_inference_compute_shared_memory_write_once");
 
     let end_time = Instant::now();
     let compute_time = end_time.duration_since(start_time).as_secs_f64();
