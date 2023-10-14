fa6dc89709809cca2c9922f4ce2809adcb955615
working <57171759+NLGithubWP@users.noreply.github.com>
2023-10-15 00:03:46 +0800

    Add test code

--- internal/pg_extension/src/bindings/inference.rs
+++ internal/pg_extension/src/bindings/inference.rs
@@ -569,7 +569,8 @@ pub fn run_sams_inference_shared_memory_write_once_int(
             }
 
             let serialized_row = serde_json::to_string(&all_rows).unwrap();
-            response_log.insert("query_data", );
+            response_log.insert("query_data", serialized_row);
+            response_log.insert("query_data2", all_rowslog);
 
             // Copy data into shared memory
             std::ptr::copy_nonoverlapping(
