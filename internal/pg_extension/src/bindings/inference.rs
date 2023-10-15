1abc2d5141743d5065845c06c93dd35072127ccf
working <57171759+NLGithubWP@users.noreply.github.com>
2023-10-15 18:52:47 +0800

    Test 1

--- internal/pg_extension/src/bindings/inference.rs
+++ internal/pg_extension/src/bindings/inference.rs
@@ -657,43 +657,23 @@ pub fn run_sams_inference_shared_memory_write_once_int(
             Ok(table) => table,
             Err(e) => return Err(e.to_string()),
         };
-
         let end_time = Instant::now();
         let data_query_time_spi = end_time.duration_since(start_time).as_secs_f64();
         response.insert("data_query_time_spi", data_query_time_spi);
 
-        let mut t1: f64 = 0.0;
         // todo: nl: this part can must be optimized, since i go through all of those staff.
         let start_time_3 = Instant::now();
-
         for i in 3..=batch as usize{
             let row = table.get(i);
-            for i in 3..=num_columns as usize {
-                let start_time_min = Instant::now();
+            for i in 3..= num_columns as usize {
                 if let Ok(Some(val)) = row.get::<i32>(i) {
                     all_rows.push(val);
                 }
-                let end_time_min = Instant::now();
-                let data_query_time_min = end_time_min.duration_since(start_time_min).as_secs_f64();
-                t1 += data_query_time_min;
             }
         }
-        // for row in table.into_iter() {
-        //     for i in 3..=num_columns as usize {
-        //         let start_time_min = Instant::now();
-        //         if let Ok(Some(val)) = row.get::<i32>(i) {
-        //             all_rows.push(val);
-        //         }
-        //         let end_time_min = Instant::now();
-        //         let data_query_time_min = end_time_min.duration_since(start_time_min).as_secs_f64();
-        //         t1 += data_query_time_min;
-        //     }
-        // }
         let end_time_min3 = Instant::now();
         let data_query_time_min3 = end_time_min3.duration_since(start_time_3).as_secs_f64();
-
         response.insert("data_query_time3", data_query_time_min3.clone());
-        response.insert("data_query_time2", t1.clone());
 
         // Return OK or some status
         Ok(())
